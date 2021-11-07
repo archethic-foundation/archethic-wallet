f7a22f46bd47b4a35bddb19af674dc648702aec3
redDwarf03 <rddwarf03@gmail.com>
2021-11-07 11:00:18 +0100

    change glovalVarEndPointDev

--- lib/global_var.dart
+++ lib/global_var.dart
@@ -1,4 +1,4 @@
 const String globalVarOriginPrivateKey =
     '01009280BDB84B8F8AEDBA205FE3552689964A5626EE2C60AA10E3BF22A91A036009';
 const String globalVarTransactionChainSeed = 'testnet';
-const String glovalVarEndPointDev = 'http://192.168.0.151:4000';
+const String glovalVarEndPointDev = 'https://testnet.archethic.net';
