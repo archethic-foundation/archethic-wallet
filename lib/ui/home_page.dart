4d2f5e215493c55d50e0c60a1f7a9338104d69f4
redDwarf03 <rddwarf03@gmail.com>
2021-12-31 13:32:29 +0100

    #14

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -5,12 +5,19 @@ import 'dart:async';
 import 'dart:typed_data';
 
 // Flutter imports:
+import 'package:archethic_mobile_wallet/styles.dart';
+import 'package:archethic_mobile_wallet/ui/transfer/transfer_uco_sheet.dart';
+import 'package:archethic_mobile_wallet/ui/widgets/custom_rect_tween.dart';
+import 'package:archethic_mobile_wallet/ui/widgets/hero_dialog_route.dart';
+import 'package:archethic_mobile_wallet/ui/widgets/sheet_util.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
 import 'package:event_taxi/event_taxi.dart';
 import 'package:flip_card/flip_card.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -20,14 +27,12 @@ import 'package:archethic_mobile_wallet/appstate_container.dart';
 import 'package:archethic_mobile_wallet/bus/events.dart';
 import 'package:archethic_mobile_wallet/localization.dart';
 import 'package:archethic_mobile_wallet/service_locator.dart';
-import 'package:archethic_mobile_wallet/ui/receive/receive_sheet.dart';
 import 'package:archethic_mobile_wallet/ui/settings/settings_drawer.dart';
 import 'package:archethic_mobile_wallet/ui/util/routes.dart';
 import 'package:archethic_mobile_wallet/ui/util/ui_util.dart';
 import 'package:archethic_mobile_wallet/ui/widgets/balance.dart';
 import 'package:archethic_mobile_wallet/ui/widgets/dialog.dart';
 import 'package:archethic_mobile_wallet/ui/widgets/line_chart.dart';
-import 'package:archethic_mobile_wallet/ui/widgets/qr_code.dart';
 import 'package:archethic_mobile_wallet/ui/widgets/tx_list.dart';
 import 'package:archethic_mobile_wallet/util/caseconverter.dart';
 import 'package:archethic_mobile_wallet/util/sharedprefsutil.dart';
@@ -49,9 +54,6 @@ class _AppHomePageState extends State<AppHomePage>
   bool? _animationDisposed;
 
   bool? _displayReleaseNote;
-
-  // Receive card instance
-  ReceiveSheet? receive;
 
   bool _lockDisabled = false; // whether we should avoid locking the app
 
@@ -169,7 +171,6 @@ class _AppHomePageState extends State<AppHomePage>
         StateContainer.of(context).recentTransactionsLoading = false;
       });
 
-      paintQrCode(event.account!.lastAddress!);
       if (event.delayPop) {
         Future<void>.delayed(const Duration(milliseconds: 300), () {
           Navigator.of(context).popUntil(RouteUtils.withNameLike('/home'));
@@ -252,30 +253,10 @@ class _AppHomePageState extends State<AppHomePage>
     }
   }
 
-  void paintQrCode(String address) {
-    final QrPainter painter = QrPainter(
-      data: address,
-      version: 6,
-      gapless: false,
-      errorCorrectionLevel: QrErrorCorrectLevel.Q,
-    );
-    painter
-        .toImageData(MediaQuery.of(context).size.width)
-        .then((ByteData? byteData) {
-      setState(() {
-        receive = ReceiveSheet(
-          qrWidget: Container(
-              width: MediaQuery.of(context).size.width / 2.675,
-              child: Image.memory(byteData!.buffer.asUint8List())),
-        );
-      });
-    });
-  }
-
   Widget _getTopCards(BuildContext context) {
     return ListView.builder(
       physics: const AlwaysScrollableScrollPhysics(),
-      itemCount: 2,
+      itemCount: 1,
       controller: _scrollController,
       scrollDirection: Axis.horizontal,
       itemBuilder: (BuildContext context, int index) {
@@ -287,20 +268,12 @@ class _AppHomePageState extends State<AppHomePage>
                 duration: const Duration(milliseconds: 200),
                 height: 120,
                 curve: Curves.easeInOut,
-                child: index == 0
-                    ? FlipCard(
-                        flipOnTouch: true,
-                        direction: FlipDirection.HORIZONTAL,
-                        front: BalanceDisplay.buildBalanceUCODisplay(
-                            context, _opacityAnimation!),
-                        back: LineChartWidget.buildTinyCoinsChart(context))
-                    : FlipCard(
-                        flipOnTouch: true,
-                        direction: FlipDirection.HORIZONTAL,
-                        front: QRcodeDisplay.buildAddressDisplay(
-                            context, _opacityAnimation!),
-                        back: QRcodeDisplay.buildQRCodeDisplay(
-                            context, _opacityAnimation!))),
+                child: FlipCard(
+                    flipOnTouch: true,
+                    direction: FlipDirection.HORIZONTAL,
+                    front: BalanceDisplay.buildBalanceUCODisplay(
+                        context, _opacityAnimation!),
+                    back: LineChartWidget.buildTinyCoinsChart(context))),
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(20.0)),
           ),
@@ -333,7 +306,55 @@ class _AppHomePageState extends State<AppHomePage>
         backgroundColor: Colors.transparent,
         elevation: 0.0,
         centerTitle: true,
-        actions: const <Widget>[],
+        actions: <Widget>[
+          if (StateContainer.of(context).wallet != null &&
+              StateContainer.of(context).wallet!.accountBalance.uco != null &&
+              StateContainer.of(context).wallet!.accountBalance.uco! > 0)
+            Padding(
+              padding: EdgeInsets.only(right: 0.0),
+              child: TextButton(
+                onPressed: () {
+                  Sheets.showAppHeightNineSheet(
+                      context: context,
+                      widget: TransferUcoSheet(
+                          contactsRef: StateContainer.of(context).contactsRef,
+                          title: AppLocalization.of(context)!.transferUCO,
+                          localCurrency:
+                              StateContainer.of(context).curCurrency));
+                },
+                child: Container(
+                    width: 20,
+                    height: 20,
+                    child: Image.asset('assets/icons/send.png',
+                        color: Colors.white)),
+              ),
+            )
+          else
+            const SizedBox(),
+          StateContainer.of(context).selectedAccount.lastAddress == null
+              ? const SizedBox()
+              : Padding(
+                  padding: EdgeInsets.only(right: 0.0),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        HeroDialogRoute(
+                          builder: (context) => Center(
+                            child: _QrCodePopupCard(),
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                        width: 20,
+                        height: 20,
+                        child: Hero(
+                            tag: 'qrcode',
+                            child: Image.asset('assets/icons/barcode.png',
+                                color: Colors.white))),
+                  ),
+                ),
+        ],
         iconTheme:
             IconThemeData(color: StateContainer.of(context).curTheme.primary),
       ),
@@ -407,5 +428,113 @@ class _AppHomePageState extends State<AppHomePage>
         await sl.get<SharedPrefsUtil>().setVersionApp(packageInfo.version);
       });
     });
+  }
+}
+
+class _QrCodePopupCard extends StatelessWidget {
+  const _QrCodePopupCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: 'qrcode',
+      createRectTween: (begin, end) {
+        return CustomRectTween(begin: begin!, end: end!);
+      },
+      child: TextButton(
+        onPressed: () {
+          Clipboard.setData(
+              ClipboardData(text: StateContainer.of(context).wallet!.address));
+          UIUtil.showSnackbar('Address copied', context);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Material(
+            color: StateContainer.of(context).curTheme.backgroundDarkest,
+            borderRadius: BorderRadius.circular(16),
+            child: SizedBox(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 200,
+                        margin: const EdgeInsets.all(8),
+                        alignment: Alignment.center,
+                        child: Text(
+                          AppLocalization.of(context)!.addressInfos,
+                          style: AppStyles.textStyleSize16W700Primary(context),
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        height: 150,
+                        alignment: Alignment.center,
+                        margin: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: StateContainer.of(context)
+                              .curTheme
+                              .backgroundDarkest,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: QrImage(
+                          foregroundColor: Colors.white,
+                          data: StateContainer.of(context)
+                              .selectedAccount
+                              .lastAddress!,
+                          version: QrVersions.auto,
+                          size: 150.0,
+                          gapless: false,
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        alignment: Alignment.center,
+                        margin: const EdgeInsets.all(8),
+                        child: Column(
+                          children: <Widget>[
+                            AutoSizeText(
+                                StateContainer.of(context)
+                                    .selectedAccount
+                                    .lastAddress!
+                                    .substring(0, 16),
+                                style: AppStyles.textStyleSize14W100Primary(
+                                    context)),
+                            AutoSizeText(
+                                StateContainer.of(context)
+                                    .selectedAccount
+                                    .lastAddress!
+                                    .substring(16, 32),
+                                style: AppStyles.textStyleSize14W100Primary(
+                                    context)),
+                            AutoSizeText(
+                                StateContainer.of(context)
+                                    .selectedAccount
+                                    .lastAddress!
+                                    .substring(32, 48),
+                                style: AppStyles.textStyleSize14W100Primary(
+                                    context)),
+                            AutoSizeText(
+                                StateContainer.of(context)
+                                    .selectedAccount
+                                    .lastAddress!
+                                    .substring(48),
+                                style: AppStyles.textStyleSize14W100Primary(
+                                    context)),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
