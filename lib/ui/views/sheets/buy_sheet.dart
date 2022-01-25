4f3df99d462264d4b85a22873fbbdffa956dc274
redDwarf03 <rddwarf03@gmail.com>
2022-01-25 23:37:31 +0100

    fix ui in web mode

--- lib/ui/views/sheets/buy_sheet.dart
+++ lib/ui/views/sheets/buy_sheet.dart
@@ -105,117 +105,113 @@ class _BuySheetState extends State<BuySheet> {
                       bottom: MediaQuery.of(context).size.height * 0.035,
                       top: 50,
                     ),
-                    child: Expanded(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: <Widget>[
-                            InkWell(
-                              onTap: () async {
-                                UIUtil.showWebview(
-                                    context,
-                                    'https://rubic.exchange/?fromChain=ETH&toChain=ETH&from=ETH&to=UCO',
-                                    'Rubic');
-                              },
-                              child: SvgPicture.asset(
-                                'assets/buy/Rubic.svg',
-                                color: Colors.white,
-                                height: 40,
-                              ),
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          InkWell(
+                            onTap: () async {
+                              UIUtil.showWebview(
+                                  context,
+                                  'https://rubic.exchange/?fromChain=ETH&toChain=ETH&from=ETH&to=UCO',
+                                  'Rubic');
+                            },
+                            child: SvgPicture.asset(
+                              'assets/buy/Rubic.svg',
+                              color: Colors.white,
+                              height: 40,
                             ),
-                            const SizedBox(height: 30),
-                            InkWell(
-                              onTap: () async {
-                                UIUtil.showWebview(
-                                    context,
-                                    'https://rubic.exchange/?fromChain=ETH&toChain=ETH&from=ETH&to=UCO',
-                                    'Quick');
-                              },
-                              child: Image.asset(
-                                'assets/buy/Quickswap.png',
-                                color: Colors.white,
-                                height: 40,
-                              ),
+                          ),
+                          const SizedBox(height: 30),
+                          InkWell(
+                            onTap: () async {
+                              UIUtil.showWebview(
+                                  context,
+                                  'https://rubic.exchange/?fromChain=ETH&toChain=ETH&from=ETH&to=UCO',
+                                  'Quick');
+                            },
+                            child: Image.asset(
+                              'assets/buy/Quickswap.png',
+                              color: Colors.white,
+                              height: 40,
                             ),
-                            const SizedBox(height: 30),
-                            InkWell(
-                              onTap: () async {
-                                UIUtil.showWebview(context,
-                                    'https://www.probit.com/en-us/', 'Probit');
-                              },
-                              child: Image.asset(
-                                'assets/buy/Probit.png',
-                                color: Colors.white,
-                                height: 40,
-                              ),
+                          ),
+                          const SizedBox(height: 30),
+                          InkWell(
+                            onTap: () async {
+                              UIUtil.showWebview(context,
+                                  'https://www.probit.com/en-us/', 'Probit');
+                            },
+                            child: Image.asset(
+                              'assets/buy/Probit.png',
+                              color: Colors.white,
+                              height: 40,
                             ),
-                            const SizedBox(height: 30),
-                            InkWell(
-                              onTap: () async {
-                                UIUtil.showWebview(
-                                    context,
-                                    'https://www.bitglobal.com/en-us',
-                                    'Bithumb');
-                              },
-                              child: Image.asset(
-                                'assets/buy/Bithumb.png',
-                                color: Colors.white,
-                                height: 40,
-                              ),
+                          ),
+                          const SizedBox(height: 30),
+                          InkWell(
+                            onTap: () async {
+                              UIUtil.showWebview(context,
+                                  'https://www.bitglobal.com/en-us', 'Bithumb');
+                            },
+                            child: Image.asset(
+                              'assets/buy/Bithumb.png',
+                              color: Colors.white,
+                              height: 40,
                             ),
-                            const SizedBox(height: 30),
-                            InkWell(
-                              onTap: () async {
-                                UIUtil.showWebview(
-                                    context,
-                                    'https://app.uniswap.org/#/swap?outputCurrency=0x8a3d77e9d6968b780564936d15b09805827c21fa&use=V2',
-                                    'Bithumb');
-                              },
-                              child: SvgPicture.asset(
-                                'assets/buy/Uniswap.svg',
-                                color: Colors.white,
-                                height: 40,
-                              ),
+                          ),
+                          const SizedBox(height: 30),
+                          InkWell(
+                            onTap: () async {
+                              UIUtil.showWebview(
+                                  context,
+                                  'https://app.uniswap.org/#/swap?outputCurrency=0x8a3d77e9d6968b780564936d15b09805827c21fa&use=V2',
+                                  'Bithumb');
+                            },
+                            child: SvgPicture.asset(
+                              'assets/buy/Uniswap.svg',
+                              color: Colors.white,
+                              height: 40,
                             ),
-                            const SizedBox(height: 30),
-                            InkWell(
-                              onTap: () async {
-                                UIUtil.showWebview(
-                                    context, 'https://zebitex.com', 'Zebitex');
-                              },
-                              child: SvgPicture.asset(
-                                'assets/buy/Zebitex.svg',
-                                color: Colors.white,
-                                height: 40,
-                              ),
+                          ),
+                          const SizedBox(height: 30),
+                          InkWell(
+                            onTap: () async {
+                              UIUtil.showWebview(
+                                  context, 'https://zebitex.com', 'Zebitex');
+                            },
+                            child: SvgPicture.asset(
+                              'assets/buy/Zebitex.svg',
+                              color: Colors.white,
+                              height: 40,
                             ),
-                            const SizedBox(height: 30),
-                            InkWell(
-                              onTap: () async {
-                                UIUtil.showWebview(
-                                    context,
-                                    'https://pancakeswap.finance/swap?inputCurrency=0xe9e7cea3dedca5984780bafc599bd69add087d56&outputCurrency=0xb001f1E7c8bda414aC7Cf7Ecba5469fE8d24B6de',
-                                    'PancakeSwap');
-                              },
-                              child: Image.asset(
-                                'assets/buy/Pancake.png',
-                                color: Colors.white,
-                                height: 40,
-                              ),
+                          ),
+                          const SizedBox(height: 30),
+                          InkWell(
+                            onTap: () async {
+                              UIUtil.showWebview(
+                                  context,
+                                  'https://pancakeswap.finance/swap?inputCurrency=0xe9e7cea3dedca5984780bafc599bd69add087d56&outputCurrency=0xb001f1E7c8bda414aC7Cf7Ecba5469fE8d24B6de',
+                                  'PancakeSwap');
+                            },
+                            child: Image.asset(
+                              'assets/buy/Pancake.png',
+                              color: Colors.white,
+                              height: 40,
                             ),
-                            const SizedBox(height: 30),
-                            InkWell(
-                              onTap: () async {
-                                UIUtil.showWebview(context,
-                                    'https://dapps.zam.io/bridge', 'Zamio');
-                              },
-                              child: Image.asset(
-                                'assets/buy/Zamio.png',
-                                color: Colors.white,
-                                height: 40,
-                              ),
+                          ),
+                          const SizedBox(height: 30),
+                          InkWell(
+                            onTap: () async {
+                              UIUtil.showWebview(context,
+                                  'https://dapps.zam.io/bridge', 'Zamio');
+                            },
+                            child: Image.asset(
+                              'assets/buy/Zamio.png',
+                              color: Colors.white,
+                              height: 40,
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
