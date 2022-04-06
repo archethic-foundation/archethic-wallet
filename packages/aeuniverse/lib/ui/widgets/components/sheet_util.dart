6889715477454e16033bbc1b2d01507582af65fc
redDwarf03 <rddwarf03@gmail.com>
2022-04-06 22:52:09 +0200

    add sheet wave transition animation

--- packages/aeuniverse/lib/ui/widgets/components/sheet_util.dart
+++ packages/aeuniverse/lib/ui/widgets/components/sheet_util.dart
@@ -28,7 +28,29 @@ class Sheets {
     bgColor ??= StateContainer.of(context).curTheme.overlay70;
     final _AppHeightNineModalRoute<T> route = _AppHeightNineModalRoute<T>(
         builder: (BuildContext context) {
-          return widget;
+          return Container(
+            child: TweenAnimationBuilder(
+              tween: Tween(begin: 0.0, end: 1.0),
+              duration: Duration(milliseconds: 500),
+              builder: (context, double value, child) {
+                return ShaderMask(
+                    shaderCallback: (rect) {
+                      return RadialGradient(
+                              radius: (value * 5),
+                              colors: [
+                                Colors.white,
+                                Colors.white,
+                                Colors.transparent,
+                                Colors.transparent
+                              ],
+                              stops: [0.0, 0.55, 0.6, 1.0],
+                              center: FractionalOffset(0.95, 0.95))
+                          .createShader(rect);
+                    },
+                    child: widget);
+              },
+            ),
+          );
         },
         color: color!,
         radius: radius,
