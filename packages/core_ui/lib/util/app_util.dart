dc11a939f5ce5d9fb6c70f36451558a89ba84e32
redDwarf03 <rddwarf03@gmail.com>
2022-04-17 09:12:36 +0200

    #73

--- packages/core_ui/lib/util/app_util.dart
+++ packages/core_ui/lib/util/app_util.dart
@@ -8,11 +8,24 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
-import 'package:archethic_lib_dart/archethic_lib_dart.dart';
+import 'package:bip32/bip32.dart' as bip32;
+import 'package:bip39/bip39.dart' as bip39;
+import 'package:archethic_lib_dart/archethic_lib_dart.dart'
+    show uint8ListToHex, deriveAddress;
 import 'package:core/localization.dart';
 import 'package:core/model/data/appdb.dart';
 import 'package:core/model/data/hive_db.dart';
 import 'package:core/util/get_it_instance.dart';
+
+/// purpose = 44' (BIP44)
+/// coin_type = 650' (UCO of ArchEthic Blockchain)
+/// account = Depends on the intended use of the key.
+/// Currently fixed values: 0xFFFF for o_{key}, 0x0000 for a simple w_{key})
+/// change = 0 (0 for external, 1 for internal)
+/// address_index = variable (0,1,2,...)
+/// path = m/44'/650'/0'/0'/0'
+const int kPurpose = 44;
+const int kCoinType = 650;
 
 class AppUtil {
   Future<Account> loginAccount(String seed, BuildContext context,
@@ -23,7 +36,8 @@ class AppUtil {
     Account? selectedAcct = await sl.get<DBHelper>().getSelectedAccount();
 
     if (selectedAcct == null) {
-      final String genesisAddress = deriveAddress(seed, 0);
+      String privateKey = seedToPrivateKey(seed, 0, 0, 0);
+      final String genesisAddress = deriveAddress(privateKey, 0);
       selectedAcct = Account(
           index: 0,
           lastAccess: 0,
@@ -33,6 +47,40 @@ class AppUtil {
       await sl.get<DBHelper>().saveAccount(selectedAcct);
     }
     return selectedAcct;
+  }
+
+  String seedToPublicKey(
+      String seed, int account, int change, int addressIndex) {
+    return uint8ListToHex(bip32.BIP32
+        .fromSeed(bip39.mnemonicToSeed(bip39.entropyToMnemonic(seed)))
+        .derivePath("m/" +
+            kPurpose.toString() +
+            "'/" +
+            kCoinType.toString() +
+            "'/" +
+            account.toString() +
+            "'/" +
+            change.toString() +
+            "/" +
+            addressIndex.toString())
+        .publicKey);
+  }
+
+  String seedToPrivateKey(
+      String seed, int account, int change, int addressIndex) {
+    return uint8ListToHex(bip32.BIP32
+        .fromSeed(bip39.mnemonicToSeed(bip39.entropyToMnemonic(seed)))
+        .derivePath("m/" +
+            kPurpose.toString() +
+            "'/" +
+            kCoinType.toString() +
+            "'/" +
+            account.toString() +
+            "'/" +
+            change.toString() +
+            "/" +
+            addressIndex.toString())
+        .privateKey!);
   }
 
   static bool isDesktopMode() {
