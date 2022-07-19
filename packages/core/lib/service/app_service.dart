bf8d2c089f34e2bd5fa02291a9c49079c594580b
redDwarf03 <rddwarf03@gmail.com>
2022-07-19 11:49:32 +0200

    Transactions inputs loading error #205

--- packages/core/lib/service/app_service.dart
+++ packages/core/lib/service/app_service.dart
@@ -62,21 +62,11 @@ class AppService {
     final List<Transaction> transactionChain = await getTransactionChain(
         lastAddress,
         pagingAddress,
-        'address, type, validationStamp { timestamp, ledgerOperations { fee } }, data { content , ledger { uco { transfers { amount, to } } } } ');
+        'address, type, validationStamp { timestamp, ledgerOperations { fee } }, data { content , ledger { uco { transfers { amount, to } } } }, inputs { from, type, spent, tokenAddress, amount, timestamp } ');
 
     final List<TransactionInput> transactionInputsGenesisAddress =
         await getTransactionInputs(genesisAddress,
             'from, type, spent, tokenAddress, amount, timestamp');
-    if (lastAddress != genesisAddress) {
-      final List<TransactionInput> transactionInputLastAddress =
-          await getTransactionInputs(
-              lastAddress, 'from, type, tokenAddress, amount, timestamp');
-      transactionInputLastAddress.forEach((TransactionInput transactionInput) {
-        if (transactionInput.from != lastAddress) {
-          transactionInputsGenesisAddress.add(transactionInput);
-        }
-      });
-    }
 
     final List<RecentTransaction> recentTransactions =
         List<RecentTransaction>.empty(growable: true);
