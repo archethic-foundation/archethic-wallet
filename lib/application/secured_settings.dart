ca70bc5b45bc99634a99d33bf929ac4494d1bbb6
redDwarf03 <rddwarf03@gmail.com>
2022-10-07 15:31:36 +0200

    add provider for Vault

--- lib/application/secured_settings.dart
+++ lib/application/secured_settings.dart
@@ -1,9 +1,6 @@
-import 'package:aewallet/model/authentication_method.dart';
-import 'package:aewallet/model/available_themes.dart';
+/// SPDX-License-Identifier: AGPL-3.0-or-later
 import 'package:aewallet/model/data/secured_settings.dart';
 import 'package:aewallet/model/data/settings.dart';
-import 'package:aewallet/model/device_lock_timeout.dart';
-import 'package:aewallet/model/device_unlock_option.dart';
 import 'package:aewallet/util/vault.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
