69d9b823db8b556f249621764222f5b4aa27ae7e
redDwarf03 <rddwarf03@gmail.com>
2022-10-20 14:36:27 +0200

    add Card widget to simplify code

--- lib/ui/views/transfer/layouts/components/uco_transfer_detail.dart
+++ lib/ui/views/transfer/layouts/components/uco_transfer_detail.dart
@@ -8,6 +8,7 @@ import 'package:aewallet/model/address.dart';
 import 'package:aewallet/ui/util/amount_formatters.dart';
 import 'package:aewallet/ui/util/styles.dart';
 import 'package:aewallet/ui/views/transfer/bloc/provider.dart';
+import 'package:aewallet/ui/views/transfer/layouts/components/transfer_detail_card.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 // Package imports:
 import 'package:flutter/material.dart';
@@ -42,219 +43,97 @@ class UCOTransferDetail extends ConsumerWidget {
               ),
             ),
           ),
-          SizedBox(
-            height: 50,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: theme.backgroundTransferListOutline!,
-                ),
-                borderRadius: BorderRadius.circular(10),
+          TransferDetailCard(
+            children: [
+              Text(
+                '${localizations.txListFrom} ${accountSelected!.name!}',
+                style: theme.textStyleSize12W400Primary,
               ),
-              elevation: 0,
-              color: theme.backgroundTransferListCard,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 10,
-                  right: 10,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      '${localizations.txListFrom} ${accountSelected!.name!}',
-                      style: theme.textStyleSize12W400Primary,
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            ],
           ),
-          SizedBox(
-            height: 50,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: theme.backgroundTransferListOutline!,
-                ),
-                borderRadius: BorderRadius.circular(10),
+          TransferDetailCard(
+            children: [
+              Text(
+                transfer.contactRecipient == null
+                    ? '${localizations.txListTo} ${Address(transfer.addressRecipient).getShortString()}'
+                    : '${localizations.txListTo} ${transfer.contactRecipient!.name!.replaceFirst('@', '')}',
+                style: theme.textStyleSize12W400Primary,
               ),
-              elevation: 0,
-              color: theme.backgroundTransferListCard,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 10,
-                  right: 10,
+              Text(
+                AmountFormatters.standard(
+                  transfer.amount,
+                  transfer.symbol,
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      transfer.contactRecipient == null
-                          ? '${localizations.txListTo} ${Address(transfer.addressRecipient).getShortString()}'
-                          : '${localizations.txListTo} ${transfer.contactRecipient!.name!.replaceFirst('@', '')}',
-                      style: theme.textStyleSize12W400Primary,
-                    ),
-                    Text(
-                      AmountFormatters.standard(
-                        transfer.amount,
-                        transfer.symbol,
-                      ),
-                      style: theme.textStyleSize12W400Primary,
-                    ),
-                  ],
-                ),
+                style: theme.textStyleSize12W400Primary,
               ),
-            ),
+            ],
           ),
-          SizedBox(
-            height: 50,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: theme.backgroundTransferListOutline!,
-                ),
-                borderRadius: BorderRadius.circular(10),
+          TransferDetailCard(
+            children: [
+              Text(
+                localizations.estimatedFees,
+                style: theme.textStyleSize12W400Primary,
               ),
-              elevation: 0,
-              color: theme.backgroundTransferListCard,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 10,
-                  right: 10,
+              Text(
+                AmountFormatters.standardSmallValue(
+                  transfer.feeEstimation,
+                  StateContainer.of(context)
+                      .curNetwork
+                      .getNetworkCryptoCurrencyLabel(),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      localizations.estimatedFees,
-                      style: theme.textStyleSize12W400Primary,
-                    ),
-                    Text(
-                      AmountFormatters.standardSmallValue(
-                        transfer.feeEstimation,
-                        StateContainer.of(context)
-                            .curNetwork
-                            .getNetworkCryptoCurrencyLabel(),
-                      ),
-                      style: theme.textStyleSize12W400Primary,
-                    ),
-                  ],
-                ),
+                style: theme.textStyleSize12W400Primary,
               ),
-            ),
+            ],
           ),
-          SizedBox(
-            height: 50,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: theme.backgroundTransferListOutline!,
-                ),
-                borderRadius: BorderRadius.circular(10),
+          TransferDetailCard(
+            children: [
+              Text(
+                localizations.total,
+                style: theme.textStyleSize12W400Primary,
               ),
-              elevation: 0,
-              color: theme.backgroundTransferListTotalCard,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 10,
-                  right: 10,
+              Text(
+                AmountFormatters.standard(
+                  transfer.feeEstimation + transfer.amount,
+                  StateContainer.of(context)
+                      .curNetwork
+                      .getNetworkCryptoCurrencyLabel(),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      localizations.total,
-                      style: theme.textStyleSize12W400Primary,
-                    ),
-                    Text(
-                      AmountFormatters.standard(
-                        transfer.feeEstimation + transfer.amount,
-                        StateContainer.of(context)
-                            .curNetwork
-                            .getNetworkCryptoCurrencyLabel(),
-                      ),
-                      style: theme.textStyleSize12W400Primary,
-                    ),
-                  ],
-                ),
+                style: theme.textStyleSize12W400Primary,
               ),
-            ),
+            ],
           ),
-          SizedBox(
-            height: 50,
-            child: Card(
-              shape: RoundedRectangleBorder(
-                side: BorderSide(
-                  color: theme.backgroundTransferListOutline!,
-                ),
-                borderRadius: BorderRadius.circular(10),
+          TransferDetailCard(
+            children: [
+              Text(
+                localizations.availableAfterTransfer,
+                style: theme.textStyleSize12W400Primary,
               ),
-              elevation: 0,
-              color: theme.backgroundTransferListCard,
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 10,
-                  right: 10,
+              Text(
+                AmountFormatters.standard(
+                  accountSelected.balance!.nativeTokenValue! -
+                      (transfer.feeEstimation + transfer.amount),
+                  transfer.symbol,
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      localizations.availableAfterTransfer,
-                      style: theme.textStyleSize12W400Primary,
-                    ),
-                    Text(
-                      AmountFormatters.standard(
-                        accountSelected.balance!.nativeTokenValue! -
-                            (transfer.feeEstimation + transfer.amount),
-                        transfer.symbol,
-                      ),
-                      style: theme.textStyleSize12W400Primary,
-                    ),
-                  ],
-                ),
+                style: theme.textStyleSize12W400Primary,
               ),
-            ),
+            ],
           ),
           if (transfer.message.isNotEmpty)
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(
-                    color: theme.backgroundTransferListOutline!,
-                  ),
-                  borderRadius: BorderRadius.circular(10),
+            TransferDetailCard(
+              children: [
+                Text(
+                  localizations.sendMessageConfirmHeader,
+                  style: theme.textStyleSize12W400Primary,
                 ),
-                elevation: 0,
-                color: theme.backgroundTransferListCard,
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: 10,
-                    bottom: 10,
-                    left: 10,
-                    right: 10,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        localizations.sendMessageConfirmHeader,
-                        style: theme.textStyleSize12W400Primary,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        transfer.message,
-                        style: theme.textStyleSize12W400Primary,
-                      ),
-                    ],
-                  ),
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-            )
+                Text(
+                  transfer.message,
+                  style: theme.textStyleSize12W400Primary,
+                ),
+              ],
+            ),
         ],
       ),
     );
