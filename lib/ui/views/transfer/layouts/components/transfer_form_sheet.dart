2df1af2e81c874e47531f0f953b02bf96840c588
redDwarf03 <rddwarf03@gmail.com>
2022-10-28 16:20:48 +0200

    Create fee informations widget

--- lib/ui/views/transfer/layouts/components/transfer_form_sheet.dart
+++ lib/ui/views/transfer/layouts/components/transfer_form_sheet.dart
@@ -1,11 +1,8 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 import 'package:aewallet/application/currency.dart';
-import 'package:aewallet/application/theme.dart';
 import 'package:aewallet/appstate_container.dart';
 import 'package:aewallet/localization.dart';
-import 'package:aewallet/ui/util/amount_formatters.dart';
 import 'package:aewallet/ui/util/dimens.dart';
-import 'package:aewallet/ui/util/styles.dart';
 import 'package:aewallet/ui/views/transfer/bloc/provider.dart';
 import 'package:aewallet/ui/views/transfer/bloc/state.dart';
 import 'package:aewallet/ui/views/transfer/layouts/transfer_sheet.dart';
@@ -14,10 +11,9 @@ import 'package:aewallet/ui/widgets/components/app_button.dart';
 import 'package:aewallet/ui/widgets/components/network_indicator.dart';
 import 'package:aewallet/ui/widgets/components/sheet_header.dart';
 import 'package:aewallet/ui/widgets/components/tap_outside_unfocus.dart';
-import 'package:aewallet/util/currency_util.dart';
+import 'package:aewallet/ui/widgets/fees/fee_infos.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class TransferFormSheet extends ConsumerWidget {
   const TransferFormSheet({
@@ -39,7 +35,6 @@ class TransferFormSheet extends ConsumerWidget {
         .appWallet!
         .appKeychain!
         .getAccountSelected()!;
-    final theme = ref.watch(ThemeProviders.selectedTheme);
     final currency = ref.watch(CurrencyProviders.selectedCurrency);
     final transfer = ref.watch(TransferFormProvider.transferForm);
     final transferNotifier =
@@ -81,49 +76,12 @@ class TransferFormSheet extends ConsumerWidget {
                                 seed: seed,
                               ),
                             ),
-                            Container(
-                              height: 40,
-                              margin: const EdgeInsets.symmetric(
-                                horizontal: 30,
-                              ),
-                              child: transfer.feeEstimation.maybeWhen(
-                                loading: () => Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      localizations
-                                          .estimatedFeesCalculationNote,
-                                      style: theme.textStyleSize14W100Primary,
-                                      textAlign: TextAlign.center,
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                        left: 3,
-                                        top: 10,
-                                      ),
-                                      child: LoadingAnimationWidget
-                                          .prograssiveDots(
-                                        color: theme.text!,
-                                        size: 12,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                data: (data) {
-                                  if (data > 0) {
-                                    return Text(
-                                      '+ ${localizations.estimatedFees}: ${AmountFormatters.standardSmallValue(data, StateContainer.of(context).curNetwork.getNetworkCryptoCurrencyLabel())}\n(${CurrencyUtil.convertAmountFormatedWithNumberOfDigits(currency.currency.name, accountSelected.balance!.tokenPrice!.amount!, transfer.feeEstimation.valueOrNull ?? 0, 8)})',
-                                      style: theme.textStyleSize14W100Primary,
-                                      textAlign: TextAlign.center,
-                                    );
-                                  }
-                                  return Text(
-                                    localizations.estimatedFeesNote,
-                                    style: theme.textStyleSize14W100Primary,
-                                  );
-                                },
-                                orElse: SizedBox.new,
-                              ),
+                            FeeInfos(
+                              feeEstimation: transfer.feeEstimation,
+                              tokenPrice:
+                                  accountSelected.balance!.tokenPrice!.amount ??
+                                      0,
+                              currencyName: currency.currency.name,
                             ),
                             const SizedBox(height: 10),
                             TransferTextFieldMessage(
---
+++ lib/ui/widgets/fees/fee_infos.dart
@@ -0,0 +1,72 @@
+/// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import 'package:aewallet/application/theme.dart';
+import 'package:aewallet/appstate_container.dart';
+import 'package:aewallet/localization.dart';
+import 'package:aewallet/ui/util/amount_formatters.dart';
+import 'package:aewallet/ui/util/styles.dart';
+import 'package:aewallet/util/currency_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+class FeeInfos extends ConsumerWidget {
+  const FeeInfos({
+    required this.feeEstimation,
+    required this.tokenPrice,
+    required this.currencyName,
+    super.key,
+  });
+
+  final AsyncValue<double> feeEstimation;
+  final double tokenPrice;
+  final String currencyName;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(ThemeProviders.selectedTheme);
+    final localizations = AppLocalization.of(context)!;
+    return Container(
+      height: 40,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 30,
+      ),
+      child: feeEstimation.maybeWhen(
+        loading: () => Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              localizations.estimatedFeesCalculationNote,
+              style: theme.textStyleSize14W100Primary,
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 3,
+                top: 10,
+              ),
+              child: LoadingAnimationWidget.prograssiveDots(
+                color: theme.text!,
+                size: 12,
+              ),
+            ),
+          ],
+        ),
+        data: (data) {
+          if (data > 0) {
+            return Text(
+              '+ ${localizations.estimatedFees}: ${AmountFormatters.standardSmallValue(data, StateContainer.of(context).curNetwork.getNetworkCryptoCurrencyLabel())}\n(${CurrencyUtil.convertAmountFormatedWithNumberOfDigits(currencyName, tokenPrice, feeEstimation.valueOrNull ?? 0, 8)})',
+              style: theme.textStyleSize14W100Primary,
+              textAlign: TextAlign.center,
+            );
+          }
+          return Text(
+            localizations.estimatedFeesNote,
+            style: theme.textStyleSize14W100Primary,
+          );
+        },
+        orElse: SizedBox.new,
+      ),
+    );
+  }
+}
