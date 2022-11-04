f1c54eb95efa2f376f836cde8f327bd90c92e8fb
redDwarf03 <rddwarf03@gmail.com>
2022-11-04 08:03:01 +0100

    add nft header widget

---
+++ lib/ui/views/nft/nft_header.dart
@@ -0,0 +1,92 @@
+/// SPDX-License-Identifier: AGPL-3.0-or-later
+import 'package:aewallet/application/account.dart';
+import 'package:aewallet/application/nft_category.dart';
+import 'package:aewallet/application/theme.dart';
+import 'package:aewallet/ui/util/styles.dart';
+import 'package:aewallet/ui/widgets/balance/balance_indicator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class NFTHeader extends ConsumerWidget {
+  const NFTHeader(
+      {super.key,
+      required this.currentNftCategoryIndex,
+      this.displayCategoryName = false});
+  final int currentNftCategoryIndex;
+  final bool displayCategoryName;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(ThemeProviders.selectedTheme);
+    final accountSelected =
+        ref.read(AccountProviders.getSelectedAccount(context: context));
+    final nftCategories = ref.read(
+      NftCategoryProviders.fetchNftCategory(
+        context: context,
+        account: accountSelected!,
+      ),
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsetsDirectional.only(
+                start: smallScreen(context) ? 15 : 20,
+              ),
+              height: 50,
+              width: 50,
+              child: BackButton(
+                key: const Key('back'),
+                color: theme.text,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ],
+        ),
+        const BalanceIndicatorWidget(
+          displaySwitchButton: false,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 10, top: 10),
+          child: Column(
+            children: [
+              Hero(
+                tag:
+                    'nftCategory${nftCategories[currentNftCategoryIndex].name!}',
+                child: Card(
+                  elevation: 5,
+                  shadowColor: Colors.black,
+                  color: theme.backgroundDark,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                    side: const BorderSide(
+                      color: Colors.white10,
+                    ),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.asset(
+                      nftCategories[currentNftCategoryIndex].image,
+                      width: 50,
+                    ),
+                  ),
+                ),
+              ),
+              if (displayCategoryName)
+                Text(
+                  nftCategories[currentNftCategoryIndex].name!,
+                  textAlign: TextAlign.center,
+                  style: theme.textStyleSize12W100Primary,
+                ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/ui/views/nft/nft_list_per_category.dart
+++ lib/ui/views/nft/nft_list_per_category.dart
@@ -3,6 +3,7 @@ import 'package:aewallet/application/account.dart';
 import 'package:aewallet/application/nft_category.dart';
 import 'package:aewallet/application/theme.dart';
 import 'package:aewallet/ui/util/styles.dart';
+import 'package:aewallet/ui/views/nft/nft_header.dart';
 import 'package:aewallet/ui/views/nft/nft_list.dart';
 import 'package:aewallet/ui/widgets/balance/balance_indicator.dart';
 import 'package:flutter/material.dart';
@@ -45,66 +46,9 @@ class NFTListPerCategory extends ConsumerWidget {
               SafeArea(
             child: Column(
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsetsDirectional.only(
-                            start: smallScreen(context) ? 15 : 20,
-                          ),
-                          height: 50,
-                          width: 50,
-                          child: BackButton(
-                            key: const Key('back'),
-                            color: theme.text,
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    const BalanceIndicatorWidget(
-                      displaySwitchButton: false,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 10, top: 10),
-                      child: Column(
-                        children: [
-                          Hero(
-                            tag:
-                                'nftCategory${nftCategories[currentNftCategoryIndex!].name!}',
-                            child: Card(
-                              elevation: 5,
-                              shadowColor: Colors.black,
-                              color: theme.backgroundDark,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15),
-                                side: const BorderSide(
-                                  color: Colors.white10,
-                                ),
-                              ),
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(15),
-                                child: Image.asset(
-                                  nftCategories[currentNftCategoryIndex!].image,
-                                  width: 50,
-                                ),
-                              ),
-                            ),
-                          ),
-                          Text(
-                            nftCategories[currentNftCategoryIndex!].name!,
-                            textAlign: TextAlign.center,
-                            style: theme.textStyleSize12W100Primary,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                NFTHeader(
+                    currentNftCategoryIndex: currentNftCategoryIndex ?? 0,
+                    displayCategoryName: true),
                 NFTList(
                   currentNftCategoryIndex: currentNftCategoryIndex,
                 ),
