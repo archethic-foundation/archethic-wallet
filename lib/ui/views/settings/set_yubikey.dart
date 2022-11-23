a44414bf6a4e7978283673360a18d3ebf3f5c29e
redDwarf03 <rddwarf03@gmail.com>
2022-11-23 09:42:14 +0100

    sort imports

--- lib/ui/views/settings/set_yubikey.dart
+++ lib/ui/views/settings/set_yubikey.dart
@@ -9,7 +9,6 @@ import 'package:aewallet/ui/util/styles.dart';
 import 'package:aewallet/ui/views/authenticate/auth_factory.dart';
 import 'package:aewallet/ui/widgets/components/app_button_tiny.dart';
 import 'package:aewallet/ui/widgets/components/app_text_field.dart';
-import 'package:aewallet/ui/widgets/components/icon_widget.dart';
 import 'package:aewallet/ui/widgets/components/icons.dart';
 import 'package:aewallet/ui/widgets/components/tap_outside_unfocus.dart';
 // Package imports:
