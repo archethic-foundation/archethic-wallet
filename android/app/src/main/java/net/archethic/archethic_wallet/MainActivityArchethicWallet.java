a4e39f074ed137f92f11cd6dbe8a07ff9d597abf
Chralu <contact@charly-caulet.net>
2022-12-19 11:39:23 +0100

    :lock: Android : Hide application preview

--- android/app/src/main/java/net/archethic/archethic_wallet/MainActivityArchethicWallet.java
+++ android/app/src/main/java/net/archethic/archethic_wallet/MainActivityArchethicWallet.java
@@ -1,6 +1,11 @@
 package net.archethic.archethic_wallet;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
@@ -8,6 +13,15 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivityArchethicWallet extends FlutterFragmentActivity {
     private static final String CHANNEL = "fappchannel";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Hides application preview (e.g. when switching between applications)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+    }
+
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
