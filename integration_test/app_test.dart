e07462fa75e99f8dbf4c5ca80dc1553e61255fe2
redDwarf03 <rddwarf03@gmail.com>
2022-12-27 14:49:06 +0100

    change test framework (maestro -> patrol)

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,54 +1,24 @@
-// Flutter imports:
-/*import 'package:flutter/material.dart';
-
-// Package imports:
-import 'package:integration_test/integration_test.dart';
-
-// Project imports:
-import 'package:aeroot/main.dart' as app;
-
-// Package imports:
+// ignore_for_file: avoid_print
+import 'package:aewallet/main.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:patrol/patrol.dart';
 
+import 'config.dart';
+
+// This is an example integration test using Patrol. Use it as a base to
+// create your own Patrol-powered test.
+//
+// To run it, you have to use `patrol drive` instead of `flutter test`.
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  testWidgets('Create new account', (WidgetTester tester) async {
-    app.main();
-    await Future.delayed(const Duration(seconds: 2));
-    await tester.pumpAndSettle();
-    final Finder buttonNewWallet = find.byKey(const Key('newWallet'));
-    await tester.tap(buttonNewWallet);
-    await Future.delayed(const Duration(seconds: 2));
-    final Finder buttonUnderstandButton =
-        find.byKey(const Key('understandButton'));
-    await tester.tap(buttonUnderstandButton);
-    await Future.delayed(const Duration(seconds: 2));
-    final Finder buttonIveBackedItUp = find.byKey(const Key('iveBackedItUp'));
-    await tester.tap(buttonIveBackedItUp);
-    await Future.delayed(const Duration(seconds: 5));
-    await tester.pump();
-    final Finder buttonYes = find.byKey(const Key('yes'));
-    await tester.tap(buttonYes);
-    await Future.delayed(const Duration(seconds: 2));
-    final Finder pinButton1 = find.byKey(const Key('pinButton1'));
-    await tester.tap(pinButton1);
-    await tester.tap(pinButton1);
-    await tester.tap(pinButton1);
-    await tester.tap(pinButton1);
-    await tester.tap(pinButton1);
-    await tester.tap(pinButton1);
-    await Future.delayed(const Duration(seconds: 2));
-    final Finder pinButtonConfirm1 = find.byKey(const Key('pinButton1'));
-    await tester.tap(pinButtonConfirm1);
-    await tester.tap(pinButtonConfirm1);
-    await tester.tap(pinButtonConfirm1);
-    await tester.tap(pinButtonConfirm1);
-    await tester.tap(pinButtonConfirm1);
-    await tester.tap(pinButtonConfirm1);
-    await Future.delayed(const Duration(seconds: 5));
-    await tester.pump();
-    expect(find.byKey(const Key('UCO')), findsOneWidget);
-  });
-}*/
+  patrolTest(
+    'test',
+    nativeAutomatorConfig: nativeAutomatorConfig,
+    nativeAutomation: true,
+    ($) async {
+      // Replace with your own app widget.
+      await $.pumpWidgetAndSettle(const App());
+    },
+  );
+}
---
+++ integration_test/config.dart
@@ -0,0 +1,8 @@
+import 'package:patrol/patrol.dart';
+
+const patrolConfig = PatrolTesterConfig();
+
+const nativeAutomatorConfig = NativeAutomatorConfig(
+  packageName: 'net.archethic.archethic_wallet',
+  bundleId: 'tech.archethic.wallet',
+);
