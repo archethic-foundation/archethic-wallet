48a063eb40e559bb8c9fb3668f3781cbcc25da99
Chralu <contact@charly-caulet.net>
2023-01-05 14:27:53 +0100

    :lipstick: ConnectivityStatusProvider cleaned

--- lib/application/connectivity_status.dart
+++ lib/application/connectivity_status.dart
@@ -3,42 +3,40 @@ import 'dart:async';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-enum ConnectivityStatus { notDetermined, isConnected, isDisconnected }
+enum ConnectivityStatus { isConnected, isDisconnected }
 
 class ConnectivityStatusNotifier extends StateNotifier<ConnectivityStatus> {
-  ConnectivityStatusNotifier() : super(ConnectivityStatus.isConnected) {
-    if (state == ConnectivityStatus.isConnected) {
-      lastResult = ConnectivityStatus.isConnected;
-    } else {
-      lastResult = ConnectivityStatus.isDisconnected;
-    }
-    lastResult = ConnectivityStatus.notDetermined;
-    Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
-      switch (result) {
-        case ConnectivityResult.mobile:
-        case ConnectivityResult.wifi:
-        case ConnectivityResult.ethernet:
-        case ConnectivityResult.bluetooth:
-        case ConnectivityResult.vpn:
-          newState = ConnectivityStatus.isConnected;
-          break;
-        case ConnectivityResult.none:
-          newState = ConnectivityStatus.isDisconnected;
-          break;
-      }
-      if (newState != lastResult) {
-        state = newState!;
-        lastResult = newState;
-      }
-    });
+  ConnectivityStatusNotifier() : super(ConnectivityStatus.isDisconnected) {
+    Connectivity().onConnectivityChanged.listen(
+      (ConnectivityResult result) {
+        final newState = result.toConnectivityStatus;
+        if (newState != state) {
+          state = newState;
+        }
+      },
+    );
   }
   StreamController<ConnectivityResult> controller =
       StreamController<ConnectivityResult>();
-
-  ConnectivityStatus? lastResult;
-  ConnectivityStatus? newState;
 }
 
-final connectivityStatusProviders = StateNotifierProvider((ref) {
-  return ConnectivityStatusNotifier();
-});
+extension _ConnectivityResultExt on ConnectivityResult {
+  ConnectivityStatus get toConnectivityStatus {
+    switch (this) {
+      case ConnectivityResult.mobile:
+      case ConnectivityResult.wifi:
+      case ConnectivityResult.ethernet:
+      case ConnectivityResult.bluetooth:
+      case ConnectivityResult.vpn:
+        return ConnectivityStatus.isConnected;
+      case ConnectivityResult.none:
+        return ConnectivityStatus.isDisconnected;
+    }
+  }
+}
+
+final connectivityStatusProviders =
+    StateNotifierProvider<ConnectivityStatusNotifier, ConnectivityStatus>(
+  (ref) => ConnectivityStatusNotifier(),
+  name: 'connectivityStatusProvider',
+);
