9cacf8099e1b88463095ae81d91427fc2b0f38a7
redDwarf03 <rddwarf03@gmail.com>
2023-01-08 21:30:17 +0100

    remove faucet feature

--- lib/localization.dart
+++ lib/localization.dart
@@ -2188,48 +2188,6 @@ class AppLocalization {
         'The connection to the network could not be completed. Please check your network settings.',
         name: 'noConnection');
   }
-
-  String get getUCOButton {
-    return Intl.message('Get UCOs', name: 'getUCOButton');
-  }
-
-  String get getUCODescription1 {
-    return Intl.message('Bring ARCHETHIC to life with your first  ',
-        name: 'getUCODescription1');
-  }
-
-  String get getUCOInformation {
-    return Intl.message(
-        'In a few minutes you will receive your UCOs which will allow you to use the Archethic network and the features of your wallet.',
-        name: 'getUCOInformation');
-  }
-
-  String get getUCOInformationAlreadyReceived {
-    return Intl.message('You have already received your UCOs.',
-        name: 'getUCOInformationAlreadyReceived');
-  }
-
-  String get getUCOInformationInsufficientBalance {
-    return Intl.message(
-        'The faucet is currently empty. Please reiterate your request tomorrow.',
-        name: 'getUCOInformationInsufficientBalance');
-  }
-
-  String get getUCOInformationUnauthorized {
-    return Intl.message(
-        'Your application is not authorized to access to the faucet. Please update your application with the last version.',
-        name: 'getUCOInformationUnauthorized');
-  }
-
-  String get getUCOInformationBackendError {
-    return Intl.message('An error occurred. Try again later.',
-        name: 'getUCOInformationBackendError');
-  }
-
-  String get getUCOCount {
-    return Intl.message('See you in HHhMM to get new UCOs...',
-        name: 'getUCOCount');
-  }
 }
 
 class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalization> {
