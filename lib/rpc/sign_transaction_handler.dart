bf82f5c76534c7b2d525e9414e47c706fbfe6299
Chralu <contact@charly-caulet.net>
2023-01-17 15:05:19 +0100

    Test JSON-RPC2 WebSocket

--- lib/rpc/sign_transaction_handler.dart
+++ lib/rpc/sign_transaction_handler.dart
@@ -1,33 +1,33 @@
 part of 'server.dart';
 
-@freezed
-class SignTransactionResult with _$SignTransactionResult {
-  const SignTransactionResult._();
+// @freezed
+// class SignTransactionResult with _$SignTransactionResult {
+//   const SignTransactionResult._();
 
-  const factory SignTransactionResult.success({
-    required int validationsCount,
-  }) = _Success;
+//   const factory SignTransactionResult.success({
+//     required int validationsCount,
+//   }) = _Success;
 
-  const factory SignTransactionResult.error() = _Error;
+//   const factory SignTransactionResult.error() = _Error;
 
-  factory SignTransactionResult.fromJson(Map<String, dynamic> json) =>
-      _$SignTransactionResultFromJson(json);
-}
+//   factory SignTransactionResult.fromJson(Map<String, dynamic> json) =>
+//       _$SignTransactionResultFromJson(json);
+// }
 
-Future<Response> _sendTransactionHandler(Request request) async {
-  log('Sign transaction command received', name: 'ArchethicRPCServer');
-  TransactionDTO transaction;
-  try {
-    final body = await request.readAsString();
-    transaction = TransactionDTO.fromJson(jsonDecode(body));
-  } catch (e) {
-    log('Bad request', error: e, name: 'Send transaction handler');
-    return Response.badRequest();
-  }
+// Future<Response> _sendTransactionHandler(Request request) async {
+//   log('Sign transaction command received', name: 'ArchethicRPCServer');
+//   TransactionDTO transaction;
+//   try {
+//     final body = await request.readAsString();
+//     transaction = TransactionDTO.fromJson(jsonDecode(body));
+//   } catch (e) {
+//     log('Bad request', error: e, name: 'Send transaction handler');
+//     return Response.badRequest();
+//   }
 
-  return Response.ok(
-    jsonEncode(
-      const SignTransactionResult.success(validationsCount: 1).toJson(),
-    ),
-  );
-}
+//   return Response.ok(
+//     jsonEncode(
+//       const SignTransactionResult.success(validationsCount: 1).toJson(),
+//     ),
+//   );
+// }
