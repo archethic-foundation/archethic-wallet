0899e2ea63f50f8658967736b23e89a5f29da601
Adrien Guinet <adrien.guinet@cabinet-roux.com>
2023-01-17 18:03:24 +0100

    557-Add Icon for transaction

--- lib/ui/views/transactions/components/transaction_detail.dart
+++ lib/ui/views/transactions/components/transaction_detail.dart
@@ -242,162 +242,182 @@ class TransactionDetail extends ConsumerWidget {
                   ),
                   Column(
                     children: <Widget>[
-                      if (transaction.typeTx == RecentTransaction.tokenCreation)
+                      Row(children: <Widget>[
+                        _TransactionIcon(
+                          transaction: transaction,
+                        ),
+                      ]),
+                    ],
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(
+                      left: 40,
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        if (transaction.typeTx ==
+                            RecentTransaction.tokenCreation)
+                          Row(
+                            children: [
+                              if (transaction.tokenInformations!.type ==
+                                  'fungible')
+                                Expanded(
+                                  child: AutoSizeText(
+                                    '${localizations.tokenCreated} ${transaction.tokenInformations!.name}',
+                                    style: theme.textStyleSize12W400Primary,
+                                  ),
+                                )
+                              else
+                                Expanded(
+                                  child: AutoSizeText(
+                                    '${localizations.nftCreated} ${transaction.tokenInformations!.name}',
+                                    style: theme.textStyleSize12W400Primary,
+                                  ),
+                                ),
+                            ],
+                          ),
+                        if (transaction.typeTx ==
+                                RecentTransaction.transferOutput ||
+                            transaction.typeTx ==
+                                RecentTransaction.tokenCreation)
+                          const SizedBox()
+                        else
+                          Row(
+                            children: <Widget>[
+                              if (transaction.from == null)
+                                const Text('')
+                              else
+                                Text(
+                                  '${localizations.txListFrom} ${Address(
+                                    transaction.contactInformations == null
+                                        ? transaction.from!
+                                        : transaction
+                                            .contactInformations!.format,
+                                  ).getShortString4()}',
+                                  style: theme.textStyleSize12W400Primary,
+                                )
+                            ],
+                          ),
+                        if (transaction.typeTx ==
+                                RecentTransaction.transferInput ||
+                            transaction.typeTx ==
+                                RecentTransaction.tokenCreation)
+                          const SizedBox()
+                        else
+                          Row(
+                            children: <Widget>[
+                              if (transaction.recipient == null)
+                                const Text('')
+                              else
+                                Text(
+                                  '${localizations.txListTo} ${Address(
+                                    transaction.contactInformations == null
+                                        ? transaction.recipient!
+                                        : transaction
+                                            .contactInformations!.format,
+                                  ).getShortString4()}',
+                                  style: theme.textStyleSize12W400Primary,
+                                )
+                            ],
+                          ),
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              DateFormat.yMMMEd(
+                                Localizations.localeOf(context).languageCode,
+                              ).add_Hms().format(
+                                    DateTime.fromMillisecondsSinceEpoch(
+                                      transaction.timestamp! * 1000,
+                                    ).toLocal(),
+                                  ),
+                              style: theme.textStyleSize12W400Primary,
+                            ),
+                          ],
+                        ),
+                        if (transaction.typeTx !=
+                            RecentTransaction.transferInput)
+                          Row(
+                            children: <Widget>[
+                              if (settings.showBalances == true)
+                                primaryCurrency.primaryCurrency ==
+                                        AvailablePrimaryCurrencyEnum.native
+                                    ? Text(
+                                        '${localizations.txListFees} ${transaction.fee!} ${AccountBalance.cryptoCurrencyLabel} (${CurrencyUtil.convertAmountFormatedWithNumberOfDigits(settings.currency.name, selectedCurrencyMarketPrice.amount, transaction.fee!, 8)})',
+                                        style: theme.textStyleSize12W400Primary,
+                                      )
+                                    : Text(
+                                        '${localizations.txListFees} ${CurrencyUtil.convertAmountFormatedWithNumberOfDigits(settings.currency.name, selectedCurrencyMarketPrice.amount, transaction.fee!, 8)} (${transaction.fee!} ${AccountBalance.cryptoCurrencyLabel})',
+                                        style: theme.textStyleSize12W400Primary,
+                                      )
+                              else
+                                Text(
+                                  '···········',
+                                  style: theme.textStyleSize12W600Primary60,
+                                )
+                            ],
+                          ),
                         Row(
                           children: [
-                            if (transaction.tokenInformations!.type ==
-                                'fungible')
-                              Expanded(
-                                child: AutoSizeText(
-                                  '${localizations.tokenCreated} ${transaction.tokenInformations!.name}',
-                                  style: theme.textStyleSize12W400Primary,
-                                ),
-                              )
-                            else
-                              Expanded(
-                                child: AutoSizeText(
-                                  '${localizations.nftCreated} ${transaction.tokenInformations!.name}',
-                                  style: theme.textStyleSize12W400Primary,
+                            if (transaction.typeTx !=
+                                    RecentTransaction.tokenCreation &&
+                                transaction.decryptedSecret != null &&
+                                transaction.decryptedSecret!.isNotEmpty)
+                              GestureDetector(
+                                onLongPressEnd: (details) {
+                                  RawInfoPopup.getPopup(
+                                    context,
+                                    ref,
+                                    details,
+                                    transaction.decryptedSecret![0],
+                                  );
+                                },
+                                child: Row(
+                                  children: [
+                                    AutoSizeText(
+                                      localizations.messageInTxTransfer,
+                                      style: theme.textStyleSize12W400Primary,
+                                    ),
+                                    const SizedBox(width: 10),
+                                    FaIcon(
+                                      FontAwesomeIcons.commentDots,
+                                      size: 12,
+                                      color: theme.text,
+                                    ),
+                                  ],
                                 ),
                               ),
                           ],
                         ),
-                      if (transaction.typeTx ==
-                              RecentTransaction.transferOutput ||
-                          transaction.typeTx == RecentTransaction.tokenCreation)
-                        const SizedBox()
-                      else
                         Row(
-                          children: <Widget>[
-                            if (transaction.from == null)
-                              const Text('')
-                            else
-                              Text(
-                                '${localizations.txListFrom} ${Address(
-                                  transaction.contactInformations == null
-                                      ? transaction.from!
-                                      : transaction.contactInformations!.format,
-                                ).getShortString4()}',
-                                style: theme.textStyleSize12W400Primary,
-                              )
-                          ],
-                        ),
-                      if (transaction.typeTx ==
-                              RecentTransaction.transferInput ||
-                          transaction.typeTx == RecentTransaction.tokenCreation)
-                        const SizedBox()
-                      else
-                        Row(
-                          children: <Widget>[
-                            if (transaction.recipient == null)
-                              const Text('')
-                            else
-                              Text(
-                                '${localizations.txListTo} ${Address(
-                                  transaction.contactInformations == null
-                                      ? transaction.recipient!
-                                      : transaction.contactInformations!.format,
-                                ).getShortString4()}',
-                                style: theme.textStyleSize12W400Primary,
-                              )
-                          ],
-                        ),
-                      Row(
-                        children: <Widget>[
-                          Text(
-                            DateFormat.yMMMEd(
-                              Localizations.localeOf(context).languageCode,
-                            ).add_Hms().format(
-                                  DateTime.fromMillisecondsSinceEpoch(
-                                    transaction.timestamp! * 1000,
-                                  ).toLocal(),
-                                ),
-                            style: theme.textStyleSize12W400Primary,
-                          ),
-                        ],
-                      ),
-                      if (transaction.typeTx != RecentTransaction.transferInput)
-                        Row(
-                          children: <Widget>[
-                            if (settings.showBalances == true)
-                              primaryCurrency.primaryCurrency ==
-                                      AvailablePrimaryCurrencyEnum.native
-                                  ? Text(
-                                      '${localizations.txListFees} ${transaction.fee!} ${AccountBalance.cryptoCurrencyLabel} (${CurrencyUtil.convertAmountFormatedWithNumberOfDigits(settings.currency.name, selectedCurrencyMarketPrice.amount, transaction.fee!, 8)})',
-                                      style: theme.textStyleSize12W400Primary,
-                                    )
-                                  : Text(
-                                      '${localizations.txListFees} ${CurrencyUtil.convertAmountFormatedWithNumberOfDigits(settings.currency.name, selectedCurrencyMarketPrice.amount, transaction.fee!, 8)} (${transaction.fee!} ${AccountBalance.cryptoCurrencyLabel})',
-                                      style: theme.textStyleSize12W400Primary,
-                                    )
-                            else
-                              Text(
-                                '···········',
-                                style: theme.textStyleSize12W600Primary60,
-                              )
-                          ],
-                        ),
-                      Row(
-                        children: [
-                          if (transaction.typeTx !=
-                                  RecentTransaction.tokenCreation &&
-                              transaction.decryptedSecret != null &&
-                              transaction.decryptedSecret!.isNotEmpty)
-                            GestureDetector(
-                              onLongPressEnd: (details) {
-                                RawInfoPopup.getPopup(
-                                  context,
-                                  ref,
-                                  details,
-                                  transaction.decryptedSecret![0],
-                                );
-                              },
-                              child: Row(
+                          children: [
+                            if (transaction.tokenInformations != null &&
+                                (kTokenFordiddenName.contains(
+                                      transaction.tokenInformations!.name!
+                                          .toUpperCase(),
+                                    ) ||
+                                    kTokenFordiddenName.contains(
+                                      transaction.tokenInformations!.symbol!
+                                          .toUpperCase(),
+                                    )))
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
                                 children: [
-                                  AutoSizeText(
-                                    localizations.messageInTxTransfer,
-                                    style: theme.textStyleSize12W400Primary,
+                                  const Icon(
+                                    UiIcons.warning,
+                                    size: 10,
                                   ),
-                                  const SizedBox(width: 10),
-                                  FaIcon(
-                                    FontAwesomeIcons.commentDots,
-                                    size: 12,
-                                    color: theme.text,
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    localizations.notOfficialUCOWarning,
+                                    style: theme.textStyleSize12W400Primary,
+                                    textAlign: TextAlign.end,
                                   ),
                                 ],
                               ),
-                            ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          if (transaction.tokenInformations != null &&
-                              (kTokenFordiddenName.contains(
-                                    transaction.tokenInformations!.name!
-                                        .toUpperCase(),
-                                  ) ||
-                                  kTokenFordiddenName.contains(
-                                    transaction.tokenInformations!.symbol!
-                                        .toUpperCase(),
-                                  )))
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                const Icon(
-                                  UiIcons.warning,
-                                  size: 10,
-                                ),
-                                const SizedBox(width: 5),
-                                Text(
-                                  localizations.notOfficialUCOWarning,
-                                  style: theme.textStyleSize12W400Primary,
-                                  textAlign: TextAlign.end,
-                                ),
-                              ],
-                            ),
-                        ],
-                      ),
-                    ],
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -406,5 +426,36 @@ class TransactionDetail extends ConsumerWidget {
         ],
       ),
     );
+  }
+}
+
+class _TransactionIcon extends ConsumerWidget {
+  const _TransactionIcon({
+    required this.transaction,
+  });
+
+  final RecentTransaction transaction;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(ThemeProviders.selectedTheme);
+
+    if (transaction.typeTx == RecentTransaction.transferInput) {
+      return Icon(
+        UiIcons.receive,
+        color: theme.text,
+        size: 30,
+      );
+    }
+
+    if (transaction.typeTx == RecentTransaction.transferOutput) {
+      return Icon(
+        UiIcons.send,
+        color: theme.text,
+        size: 30,
+      );
+    }
+
+    return Container();
   }
 }
