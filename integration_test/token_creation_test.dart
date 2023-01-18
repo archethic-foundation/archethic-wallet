c31d7654173b39b44da7c26fb805e9d07e316256
David ROCHELET <drochelet@groupeisgri.com>
2023-01-18 06:55:19 +0100

    WIP:token creation

---
+++ integration_test/token_creation_test.dart
@@ -0,0 +1,91 @@
+//@Timeout(Duration(seconds: 90))
+import 'package:aewallet/main.dart' as app;
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:path/path.dart';
+import 'package:patrol/patrol.dart';
+
+import 'config.dart';
+
+void main() {
+  patrolTest('As a user I can create a token',
+      nativeAutomatorConfig: nativeAutomatorConfig,
+      nativeAutomation: true, ($) async {
+    await app.main();
+    await $(CheckboxListTile).tap();
+    await $(#importWallet).tap();
+    final finder = createFinder(RegExp('.*testnet.*'));
+    await $(finder).tap();
+
+    final seedWord = [
+      'pave',
+      'shrug',
+      'coffee',
+      'daughter',
+      'hip',
+      'mechanic',
+      'scale',
+      'trigger',
+      'lake',
+      'resist',
+      'way',
+      'repair',
+      'good',
+      'animal',
+      'tennis',
+      'boost',
+      'walk',
+      'story',
+      'dash',
+      'brass',
+      'buzz',
+      'orphan',
+      'feed',
+      'connect'
+    ];
+
+    const length = 23;
+    for (var index = 0; index <= length; index++) {
+      final seedWordFieldFinder = createFinder(Key('seedWord$index'));
+      await $(seedWordFieldFinder).scrollTo().tap();
+      await $(seedWordFieldFinder).enterText(seedWord[index]);
+      //await $.native.pressBack();
+    }
+    await $(#seedWordsOKbutton).tap(
+      settleTimeout: const Duration(minutes: 10),
+    );
+
+    //await $(#accountName0).waitUntilVisible().tap();
+    await $(#accessModePIN).tap();
+
+    // code pin 000000 avec confirmation
+    const pinLength = 12;
+    for (var i = pinLength; i >= 1; i--) {
+      await $('0').tap(
+        settleTimeout: const Duration(minutes: 10),
+      );
+    }
+
+    await $(#tokenTab).tap();
+    await $(#tokenCreate).tap();
+    await $(#nftCreateButton).tap();
+    await $(#nftNameButton).enterText('nftTest');
+    await $(#nextButton).tap();
+    await $(#nftImportPicture).tap();
+//add mock with a smple picture
+    await $(#nextButton).tap();
+    await $(#nftName).enterText('nftTest');
+    await $(#nftValue).enterText('10');
+    await $(#nextButton).tap();
+    await $(#nftCreationConfirmation).tap();
+    await $(#nftCreationFinalConfirmation).tap();
+
+    // code pin 000000
+    const pinConfirmationLength = 6;
+    for (var i = pinConfirmationLength; i >= 1; i--) {
+      await $('0').tap(
+        settleTimeout: const Duration(minutes: 10),
+      );
+    }
+  });
+}
