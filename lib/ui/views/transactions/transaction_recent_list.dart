210927391c4c28c242776be3ff53c4e715e71cb3
Adrien Guinet <adrien.guinet@cabinet-roux.com>
2023-01-29 18:37:41 +0100

    520 Remove Duplicate Transaction

--- lib/ui/views/transactions/transaction_recent_list.dart
+++ lib/ui/views/transactions/transaction_recent_list.dart
@@ -20,60 +20,94 @@ class TxList extends ConsumerWidget {
       ),
     );
 
-    final theme = ref.watch(ThemeProviders.selectedTheme);
-
     return Column(
       children: [
         if (recentTransactions != null)
           recentTransactions.isEmpty
-              ? Container(
-                  alignment: Alignment.center,
-                  color: Colors.transparent,
-                  width: MediaQuery.of(context).size.width,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 5, right: 5, top: 6),
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide(
-                          color: theme.backgroundFungiblesTokensListCard!,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      elevation: 0,
-                      color: theme.backgroundFungiblesTokensListCard,
-                      child: Container(
-                        padding: const EdgeInsets.all(9.5),
-                        width: MediaQuery.of(context).size.width,
-                        alignment: Alignment.center,
-                        child: Row(
-                          children: [
-                            const Icon(
-                              UiIcons.about,
-                              size: 16,
-                            ),
-                            const SizedBox(width: 5),
-                            Text(
-                              AppLocalization.of(context)!
-                                  .recentTransactionsNoTransactionYet,
-                              style: theme.textStyleSize12W100Primary,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                )
-              : Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: recentTransactions
-                      .map(
-                        (recentTransaction) => _TxListLine(
-                          recentTransaction: recentTransaction,
-                        ),
-                      )
-                      .toList(),
+              ? const _EmptyTransactions()
+              : _TransactionList(
+                  recentTransactions: recentTransactions,
                 ),
       ],
+    );
+  }
+}
+
+class _EmptyTransactions extends ConsumerWidget {
+  const _EmptyTransactions();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(ThemeProviders.selectedTheme);
+
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.transparent,
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 5, right: 5, top: 6),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            side: BorderSide(
+              color: theme.backgroundFungiblesTokensListCard!,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          elevation: 0,
+          color: theme.backgroundFungiblesTokensListCard,
+          child: Container(
+            padding: const EdgeInsets.all(9.5),
+            width: MediaQuery.of(context).size.width,
+            alignment: Alignment.center,
+            child: Row(
+              children: [
+                const Icon(
+                  UiIcons.about,
+                  size: 16,
+                ),
+                const SizedBox(width: 5),
+                Text(
+                  AppLocalization.of(context)!
+                      .recentTransactionsNoTransactionYet,
+                  style: theme.textStyleSize12W100Primary,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TransactionList extends ConsumerWidget {
+  const _TransactionList({
+    required this.recentTransactions,
+  });
+
+  final List<RecentTransaction> recentTransactions;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Remove duplicate with same timestamp and address
+    final seen = <String>{};
+    final uniqueListOfTransactions = recentTransactions
+        .where(
+          (recentTransaction) => seen.add(
+            '${recentTransaction.timestamp.toString()}-${recentTransaction.address ?? ""}',
+          ),
+        )
+        .toList();
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: uniqueListOfTransactions
+          .map(
+            (recentTransaction) => _TxListLine(
+              recentTransaction: recentTransaction,
+            ),
+          )
+          .toList(),
     );
   }
 }
