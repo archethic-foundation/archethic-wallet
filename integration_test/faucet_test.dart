54e5e94a9bb6f4df13748c2673be88d53b37acdd
David ROCHELET <drochelet@groupeisgri.com>
2023-01-31 17:01:15 +0100

    Added request UCOs action and the associated test

---
+++ integration_test/faucet_test.dart
@@ -0,0 +1,12 @@
+import 'package:patrol/patrol.dart';
+import 'action/faucet_request.dart';
+
+import 'config/config.dart';
+
+void main() {
+  patrolTest('I can request 100 UCO from the faucet',
+      nativeAutomatorConfig: nativeAutomatorConfig,
+      nativeAutomation: true, ($) async {
+    await faucetRequestAction($);
+  });
+}
