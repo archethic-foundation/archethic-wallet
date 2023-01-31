2ba2b2418210653598758b19c008a66466c5f6f2
David ROCHELET <drochelet@groupeisgri.com>
2023-01-31 17:01:15 +0100

    Added request UCOs action and the associated test

---
+++ integration_test/action/faucet_request.dart
@@ -0,0 +1,32 @@
+import 'package:patrol/patrol.dart';
+import 'package:requests/requests.dart';
+
+import '../config/config.dart';
+
+Future<void> faucetRequestAction(PatrolTester $) async {
+  // Request the form
+  final formRequest =
+      await Requests.get('https://testnet.archethic.net/faucet');
+  formRequest.raiseForStatus();
+  final body = formRequest.content();
+
+  // Extract the CSRF
+  final exp = RegExp(r'^.*name=\"_csrf_token\" value=\"(.*)\".*$');
+  final matches = exp.allMatches(body).toList();
+  final csrf = matches[0];
+
+  // Save the cookies
+  final cookies = await Requests.getStoredCookies('archethic.net');
+
+  // Build the request for 100UCO
+  Requests.setStoredCookies('archethic.net', cookies);
+
+  final request100Uco = await Requests.post(
+    'https://testnet.archethic.net/faucet',
+    body: {'csrf': csrf, 'address': 100},
+  );
+
+  // Check the status
+  request100Uco.raiseForStatus();
+  assert(request100Uco.success);
+}
