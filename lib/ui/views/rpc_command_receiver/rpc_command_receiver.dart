c1c54c28bc5cf937c1b5aec696eadb6be1d682ec
redDwarf03 <rddwarf03@gmail.com>
2023-03-03 10:53:00 +0100

    Add getAccounts RPC command

--- lib/ui/views/rpc_command_receiver/rpc_command_receiver.dart
+++ lib/ui/views/rpc_command_receiver/rpc_command_receiver.dart
@@ -1,8 +1,11 @@
+import 'package:aewallet/domain/models/app_accounts.dart';
 import 'package:aewallet/domain/models/core/result.dart';
 import 'package:aewallet/domain/repositories/settings.dart';
 import 'package:aewallet/domain/rpc/command_dispatcher.dart';
 import 'package:aewallet/domain/rpc/commands/command.dart';
+import 'package:aewallet/domain/rpc/commands/get_accounts.dart';
 import 'package:aewallet/domain/rpc/commands/get_endpoint.dart';
+import 'package:aewallet/model/data/appdb.dart';
 import 'package:aewallet/ui/views/rpc_command_receiver/send_transaction/command_handler.dart';
 import 'package:aewallet/util/get_it_instance.dart';
 import 'package:flutter/material.dart';
@@ -40,6 +43,30 @@ class RPCCommandReceiver extends ConsumerWidget {
 
             return Result.success(
               RPCGetEndpointResultData(endpoint: endpointUrl),
+            );
+          },
+        ),
+      )
+      ..addHandler(
+        CommandHandler(
+          canHandle: (command) =>
+              command is RPCCommand<RPCGetAccountsCommandData>,
+          handle: (command) async {
+            command as RPCCommand<RPCGetAccountsCommandData>;
+            final _dbHelper = sl.get<DBHelper>();
+            final appAccounts = await _dbHelper.getAccounts();
+            final accounts = <AppAccount>[];
+            for (final accountAppName in appAccounts) {
+              accounts.add(
+                AppAccount(
+                  name: accountAppName.name,
+                  genesisAddress: accountAppName.genesisAddress,
+                ),
+              );
+            }
+
+            return Result.success(
+              RPCGetAccountsResultData(accounts: accounts),
             );
           },
         ),
