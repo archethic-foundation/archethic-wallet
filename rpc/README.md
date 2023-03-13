730af7a690ec1ca8d112cdd8c2835f8c10984d15
redDwarf03 <rddwarf03@gmail.com>
2023-03-13 10:52:00 +0100

    Delete unused ressources

--- rpc/README.md
+++
@@ -1,5 +0,0 @@
-# RPC stuffs
-
-This directory contains "in-progress side tools"/POCs related to Dapp RPC.
-
-In the long-term, this directory content should be deleted or moved to dedicated repositories.
--- rpc/extension/src/content/inpage.js
+++
@@ -1,7 +0,0 @@
-import { DefaultArchethicRPCClient } from 'archethic_wallet_rpc_client';
-
-function init() {
-    console.log("InpageScript start");
-    window.archethic = new DefaultArchethicRPCClient();
-    console.log("InpageScript done");
-}
