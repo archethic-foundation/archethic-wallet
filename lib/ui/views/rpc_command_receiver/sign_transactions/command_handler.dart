8eaebb97102a2c8a42cbd77a769b95b3e8d5350b
redDwarf03 <rddwarf03@gmail.com>
2023-03-25 21:24:37 +0100

    get Keychain infos directly in the BC ; not in the wallet's session

--- lib/ui/views/rpc_command_receiver/sign_transactions/command_handler.dart
+++ lib/ui/views/rpc_command_receiver/sign_transactions/command_handler.dart
@@ -4,7 +4,6 @@ import 'package:aewallet/domain/rpc/command_dispatcher.dart';
 import 'package:aewallet/domain/rpc/commands/command.dart';
 import 'package:aewallet/domain/rpc/commands/sign_transactions.dart';
 import 'package:aewallet/util/get_it_instance.dart';
-import 'package:aewallet/util/keychain_util.dart';
 import 'package:archethic_lib_dart/archethic_lib_dart.dart' as archethic;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -22,12 +21,11 @@ class SignTransactionsCommandHandler extends CommandHandler {
             final serviceName = command.data.serviceName;
             final pathSuffix = command.data.pathSuffix ?? '';
 
-            final keychain = ref
-                .read(SessionProviders.session)
-                .loggedIn!
-                .wallet
-                .keychainSecuredInfos
-                .toKeychain();
+            final seed =
+                ref.read(SessionProviders.session).loggedIn!.wallet.seed;
+
+            final keychain =
+                await sl.get<archethic.ApiService>().getKeychain(seed);
 
             final addressGenesis = archethic.uint8ListToHex(
               keychain.deriveAddress(
