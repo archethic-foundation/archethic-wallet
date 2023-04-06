1a98f51bce772ce85aba9b223785009a39e45509
redDwarf03 <rddwarf03@gmail.com>
2023-04-06 22:13:04 +0200

    Add method with extension management

--- lib/util/mime_util.dart
+++ lib/util/mime_util.dart
@@ -1,3 +1,5 @@
+import 'package:mime_dart/mime_dart.dart';
+
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 class MimeUtil {
   static List<String> imageTypeMimeImage = [
@@ -5,24 +7,72 @@ class MimeUtil {
     'image/png',
     'image/gif',
     'image/webp',
-    'image/bmp'
+    'image/bmp',
   ];
 
-  static List<String> pdfTypeMime = ['application/x-pdf', 'application/pdf'];
+  static List<String> svgTypeMimeImage = [
+    'image/svg+xml',
+  ];
+
+  static List<String> pdfTypeMime = [
+    'application/x-pdf',
+    'application/pdf',
+  ];
 
   static bool isImage(String? typeMime) {
     if (typeMime == null) {
       return false;
-    } else {
-      return imageTypeMimeImage.contains(typeMime);
     }
+    return imageTypeMimeImage.contains(typeMime);
+  }
+
+  static bool isSvg(String? typeMime) {
+    if (typeMime == null) {
+      return false;
+    }
+    return svgTypeMimeImage.contains(typeMime);
   }
 
   static bool isPdf(String? typeMime) {
     if (typeMime == null) {
       return false;
-    } else {
-      return pdfTypeMime.contains(typeMime);
     }
+    return pdfTypeMime.contains(typeMime);
+  }
+
+  static bool isSvgFromExtension(String? url) {
+    if (url == null) {
+      return false;
+    }
+
+    final extensions = Mime.getTypesFromExtension(url.split('.').last);
+    if (extensions != null && extensions.isNotEmpty) {
+      return isSvg(extensions.first);
+    }
+    return false;
+  }
+
+  static bool isImageFromExtension(String? url) {
+    if (url == null) {
+      return false;
+    }
+
+    final extensions = Mime.getTypesFromExtension(url.split('.').last);
+    if (extensions != null && extensions.isNotEmpty) {
+      return isImage(extensions.first);
+    }
+    return false;
+  }
+
+  static bool isPdfFromExtension(String? url) {
+    if (url == null) {
+      return false;
+    }
+
+    final extensions = Mime.getTypesFromExtension(url.split('.').last);
+    if (extensions != null && extensions.isNotEmpty) {
+      return isPdf(extensions.first);
+    }
+    return false;
   }
 }
