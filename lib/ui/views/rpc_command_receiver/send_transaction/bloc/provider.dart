26e9576a1c565ce991de8bfc1d6af1980da98515
redDwarf03 <rddwarf03@gmail.com>
2023-05-09 18:28:49 +0200

    Fees calculation on RPC sendTx command

--- lib/ui/views/rpc_command_receiver/send_transaction/bloc/provider.dart
+++ lib/ui/views/rpc_command_receiver/send_transaction/bloc/provider.dart
@@ -7,7 +7,6 @@ import 'package:aewallet/domain/rpc/commands/command.dart';
 import 'package:aewallet/domain/rpc/commands/send_transaction.dart';
 import 'package:aewallet/domain/usecases/transaction/send_transaction.dart';
 import 'package:aewallet/domain/usecases/usecase.dart';
-import 'package:aewallet/model/data/account.dart';
 import 'package:aewallet/util/get_it_instance.dart';
 import 'package:archethic_lib_dart/archethic_lib_dart.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -22,7 +21,7 @@ class SignTransactionConfirmationFormState
     with _$SignTransactionConfirmationFormState {
   const factory SignTransactionConfirmationFormState({
     required RPCCommand<RPCSendTransactionCommandData> signTransactionCommand,
-    required Account senderAccount,
+    required double feesEstimation,
   }) = _SignTransactionConfirmationFormState;
   const SignTransactionConfirmationFormState._();
 }
@@ -34,27 +33,28 @@ class SignTransactionConfirmationFormNotifier
   Future<SignTransactionConfirmationFormState> build(
     RPCCommand<RPCSendTransactionCommandData> arg,
   ) async {
-    final selectedAccount = await ref.read(
-      AccountProviders.selectedAccount.future,
+    final accountSelected =
+        ref.watch(AccountProviders.selectedAccount).valueOrNull;
+
+    final apiService = sl.get<ApiService>();
+    final transactionBuilt = await SendTransactionCommand(
+      senderAccount: accountSelected!,
+      data: arg.data.data,
+      type: arg.data.type,
+      version: arg.data.version,
+    ).toArchethicTransaction(
+      wallet: ref.watch(SessionProviders.session).loggedIn!.wallet,
+      apiService: apiService,
+      senderAccount: accountSelected,
     );
+
+    final feesEstimation = await apiService.getTransactionFee(
+      transactionBuilt!,
+    );
+
     return SignTransactionConfirmationFormState(
       signTransactionCommand: arg,
-      senderAccount: selectedAccount!,
-    );
-  }
-
-  void setAccount(Account account) {
-    state.maybeMap(
-      orElse: () => const Result.failure(
-        TransactionError.other(reason: 'Form is not loaded yet.'),
-      ),
-      data: (data) {
-        state = AsyncValue.data(
-          data.value.copyWith(
-            senderAccount: account,
-          ),
-        );
-      },
+      feesEstimation: fromBigInt(feesEstimation.fee).toDouble(),
     );
   }
 
@@ -65,11 +65,13 @@ class SignTransactionConfirmationFormNotifier
         orElse: () => const Result.failure(TransactionError.other()),
         data: (data) {
           final useCase = ref.read(_sendTransactionUseCaseProvider);
+          final accountSelected =
+              ref.watch(AccountProviders.selectedAccount).valueOrNull;
 
           return useCase
               .run(
             SendTransactionCommand(
-              senderAccount: data.value.senderAccount,
+              senderAccount: accountSelected!,
               data: data.value.signTransactionCommand.data.data,
               type: data.value.signTransactionCommand.data.type,
               version: data.value.signTransactionCommand.data.version,
--- lib/ui/views/rpc_command_receiver/send_transaction/layouts/send_transaction_confirmation_form.dart
+++ lib/ui/views/rpc_command_receiver/send_transaction/layouts/send_transaction_confirmation_form.dart
@@ -12,6 +12,7 @@ import 'package:aewallet/domain/usecases/usecase.dart';
 import 'package:aewallet/model/authentication_method.dart';
 import 'package:aewallet/model/data/account_balance.dart';
 import 'package:aewallet/ui/themes/themes.dart';
+import 'package:aewallet/ui/util/amount_formatters.dart';
 import 'package:aewallet/ui/util/dimens.dart';
 import 'package:aewallet/ui/util/styles.dart';
 import 'package:aewallet/ui/util/ui_util.dart';
@@ -24,14 +25,15 @@ import 'package:aewallet/ui/widgets/components/sheet_detail_card.dart';
 import 'package:aewallet/ui/widgets/components/sheet_header.dart';
 import 'package:aewallet/ui/widgets/components/show_sending_animation.dart';
 import 'package:aewallet/ui/widgets/components/tap_outside_unfocus.dart';
-import 'package:aewallet/util/number_util.dart';
-import 'package:archethic_lib_dart/archethic_lib_dart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SendTransactionConfirmationForm extends ConsumerWidget {
-  const SendTransactionConfirmationForm(this.command, {super.key});
+  const SendTransactionConfirmationForm(
+    this.command, {
+    super.key,
+  });
 
   final RPCCommand<RPCSendTransactionCommandData> command;
 
@@ -40,6 +42,8 @@ class SendTransactionConfirmationForm extends ConsumerWidget {
     final theme = ref.watch(ThemeProviders.selectedTheme);
     final localizations = AppLocalizations.of(context)!;
     final bottom = MediaQuery.of(context).viewInsets.bottom;
+    final accountSelected =
+        ref.watch(AccountProviders.selectedAccount).valueOrNull;
 
     final formState = ref.watch(
       SignTransactionConfirmationProviders.form(command),
@@ -74,19 +78,75 @@ class SendTransactionConfirmationForm extends ConsumerWidget {
                       ),
                       child: Column(
                         children: <Widget>[
-                          Text(
-                            localizations
-                                .transactionSignatureCommandReceivedNotification
-                                .replaceAll(
-                              '%1',
-                              formData.value.signTransactionCommand.origin.name,
-                            ),
-                            style: theme.textStyleSize12W400Primary,
-                          ),
                           const SizedBox(
                             height: 30,
                           ),
-                          displayInfoDetail(context, ref),
+                          Column(
+                            children: [
+                              SheetDetailCard(
+                                children: [
+                                  Text(
+                                    localizations.estimatedFees,
+                                    style: theme.textStyleSize12W400Primary,
+                                  ),
+                                  Text(
+                                    '${formData.value.feesEstimation} ${AccountBalance.cryptoCurrencyLabel}',
+                                    style: theme.textStyleSize12W400Primary,
+                                  ),
+                                ],
+                              ),
+                              SheetDetailCard(
+                                children: [
+                                  Text(
+                                    localizations.availableAfterCreation,
+                                    style: theme.textStyleSize12W400Primary,
+                                  ),
+                                  Text(
+                                    AmountFormatters.standard(
+                                      accountSelected!
+                                              .balance!.nativeTokenValue -
+                                          formData.value.feesEstimation,
+                                      AccountBalance.cryptoCurrencyLabel,
+                                    ),
+                                    style: theme.textStyleSize12W400Primary,
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                localizations
+                                    .signTransactionListTransactionsHeader
+                                    .replaceAll('%1', '')
+                                    .trim(),
+                                style: theme.textStyleSize12W400Primary,
+                              ),
+                              SizedBox.fromSize(
+                                child: Card(
+                                  shape: RoundedRectangleBorder(
+                                    side: BorderSide(
+                                      color:
+                                          theme.backgroundTransferListOutline!,
+                                    ),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  elevation: 0,
+                                  color: theme.backgroundTransferListCard,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(10),
+                                    child: SelectableText(
+                                      const JsonEncoder.withIndent('  ')
+                                          .convert(
+                                        command.data.data,
+                                      ),
+                                      style: theme.textStyleSize12W400Primary,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
                     ),
@@ -170,262 +230,6 @@ class SendTransactionConfirmationForm extends ConsumerWidget {
         );
       },
     );
-  }
-
-  Widget displayInfoDetail(BuildContext context, WidgetRef ref) {
-    final localizations = AppLocalizations.of(context)!;
-    final theme = ref.watch(ThemeProviders.selectedTheme);
-    final accountSelected =
-        ref.watch(AccountProviders.selectedAccount).valueOrNull;
-
-    switch (command.data.type) {
-      case 'keychain':
-        return Container(
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.only(left: 10, right: 10),
-          child: Column(
-            children: [
-              SheetDetailCard(
-                children: [
-                  Text(
-                    'Action',
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Expanded(
-                    child: Text(
-                      'Ajout d\'un service',
-                      style: theme.textStyleSize12W400Primary,
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                ],
-              ),
-              SheetDetailCard(
-                children: [
-                  Text(
-                    localizations.tokenName,
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Expanded(
-                    child: Text(
-                      command.data.data.toString(),
-                      style: theme.textStyleSize12W400Primary,
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                ],
-              ),
-              SheetDetailCard(
-                children: [
-                  Text(
-                    localizations.estimatedFees,
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Text(
-                    '0 ${AccountBalance.cryptoCurrencyLabel}',
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                ],
-              ),
-              /*SheetDetailCard(
-                children: [
-                  Text(
-                    localizations.availableAfterCreation,
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Text(
-                    AmountFormatters.standard(
-                      accountSelected!.balance!.nativeTokenValue,
-                      addAccount.symbolFees(context),
-                    ),
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                ],
-              ),*/
-            ],
-          ),
-        );
-
-      case 'token':
-        final token = Token.fromJson(jsonDecode(command.data.data.content!));
-        return Container(
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.only(left: 10, right: 10),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 30, right: 30),
-                child: Text(
-                  localizations.addTokenConfirmationMessage,
-                  style: theme.textStyleSize12W400Primary,
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SheetDetailCard(
-                children: [
-                  Text(
-                    localizations.tokenName,
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Expanded(
-                    child: Text(
-                      token.name ?? '',
-                      style: theme.textStyleSize12W400Primary,
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                ],
-              ),
-              SheetDetailCard(
-                children: [
-                  Text(
-                    localizations.tokenSymbol,
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Text(
-                    token.symbol ?? '',
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                ],
-              ),
-              SheetDetailCard(
-                children: [
-                  Text(
-                    localizations.tokenInitialSupply,
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Text(
-                    NumberUtil.formatThousands(token.supply ?? 0),
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      /* case 'transfer':
-        var amountInUco = command.data.data.ledger!.uco!.transfers[0].amount;
-        final primaryCurrency =
-            ref.watch(PrimaryCurrencyProviders.selectedPrimaryCurrency);
-      
-        return Container(
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.only(left: 10, right: 10),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: Align(
-                  child: AutoSizeText(
-                    AmountFormatters.standard(
-                      amountInUco,
-                      transfer.symbol(context),
-                    ),
-                    style: theme.textStyleSize28W700Primary,
-                  ),
-                ),
-              ),
-              SheetDetailCard(
-                children: [
-                  Text(
-                    '${localizations.txListFrom} ${accountSelected.name}',
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                ],
-              ),
-              SheetDetailCard(
-                children: [
-                  Text(
-                    '${localizations.txListTo} ${transfer.recipient.format(localizations)}',
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Text(
-                    AmountFormatters.standard(
-                      amountInUco,
-                      transfer.symbol(context),
-                    ),
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                ],
-              ),
-              SheetDetailCard(
-                children: [
-                  Text(
-                    localizations.estimatedFees,
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Text(
-                    AmountFormatters.standardSmallValue(
-                      transfer.feeEstimationOrZero,
-                      AccountBalance.cryptoCurrencyLabel,
-                    ),
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                ],
-              ),
-              SheetDetailCard(
-                children: [
-                  Text(
-                    localizations.total,
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Text(
-                    AmountFormatters.standard(
-                      transfer.feeEstimationOrZero + amountInUco,
-                      AccountBalance.cryptoCurrencyLabel,
-                    ),
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                ],
-              ),
-              SheetDetailCard(
-                children: [
-                  Text(
-                    localizations.availableAfterTransfer,
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                  Text(
-                    AmountFormatters.standard(
-                      accountSelected.balance!.nativeTokenValue -
-                          (transfer.feeEstimationOrZero + amountInUco),
-                      transfer.symbol(context),
-                    ),
-                    style: theme.textStyleSize12W400Primary,
-                  ),
-                ],
-              ),
-              if (transfer.message.isNotEmpty)
-                SheetDetailCard(
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            localizations.sendMessageConfirmHeader,
-                            style: theme.textStyleSize12W400Primary,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: Text(
-                              transfer.message,
-                              style: theme.textStyleSize12W400Primary,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-            ],
-          ),
-        );*/
-
-      default:
-        return const SizedBox();
-    }
   }
 
   void _showSendProgress(
