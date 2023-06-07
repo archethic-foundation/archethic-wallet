336b327e9326f72ca1a1cc249e487b96d83d3772
Chralu <contact@charly-caulet.net>
2023-06-07 18:06:05 +0200

    :sparkles: Messaging group creation form.

--- lib/domain/repositories/messenger_repository.dart
+++ lib/domain/repositories/messenger_repository.dart
@@ -3,8 +3,8 @@ import 'package:aewallet/domain/models/core/failures.dart';
 import 'package:aewallet/domain/models/core/result.dart';
 import 'package:aewallet/model/available_networks.dart';
 import 'package:aewallet/model/data/account.dart';
-import 'package:aewallet/model/data/contact.dart';
 import 'package:aewallet/model/messenger/talk.dart';
+import 'package:aewallet/model/public_key.dart';
 
 abstract class MessengerRepositoryInterface {
   Future<Result<List<String>, Failure>> getTalkIds();
@@ -12,8 +12,8 @@ abstract class MessengerRepositoryInterface {
   Future<Result<Talk, Failure>> getTalk(String talkId);
 
   Future<Result<Talk, Failure>> createTalk({
-    required List<Contact> members,
-    required List<Contact> admins,
+    required List<PublicKey> members,
+    required List<PublicKey> admins,
     required Account creator,
     required LoggedInSession session,
     required NetworksSetting networkSettings,
--- lib/ui/views/main/messenger_tab/bloc/providers.dart
+++ lib/ui/views/main/messenger_tab/bloc/providers.dart
@@ -1,64 +1,30 @@
-import 'package:aewallet/application/account/providers.dart';
-import 'package:aewallet/application/settings/settings.dart';
-import 'package:aewallet/application/wallet/wallet.dart';
-import 'package:aewallet/domain/models/core/failures.dart';
 import 'package:aewallet/domain/models/core/result.dart';
 import 'package:aewallet/domain/repositories/messenger_repository.dart';
 import 'package:aewallet/infrastructure/repositories/messenger_repository.dart';
-import 'package:aewallet/model/data/contact.dart';
 import 'package:aewallet/model/messenger/talk.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:aewallet/ui/views/main/messenger_tab/bloc/create_talk.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+
 part 'providers.g.dart';
 
 @riverpod
 Future<Talk> _talk(_TalkRef ref, String talkId) async => ref
-    .watch(MessengerProviders._messengerRepository)
+    .watch(MessengerProviders.messengerRepository)
     .getTalk(talkId)
     .valueOrThrow;
 
 @riverpod
-Future<List<String>> _talkIds(_TalkIdsRef ref) async => ref
-    .watch(MessengerProviders._messengerRepository)
-    .getTalkIds()
-    .valueOrThrow;
+Future<List<String>> _talkIds(_TalkIdsRef ref) async =>
+    ref.watch(MessengerProviders.messengerRepository).getTalkIds().valueOrThrow;
 
 abstract class MessengerProviders {
-  static final _messengerRepository = Provider<MessengerRepositoryInterface>(
+  static final messengerRepository = Provider<MessengerRepositoryInterface>(
     (ref) => MessengerRepository(),
   );
 
   static final talkIds = _talkIdsProvider;
 
-  static final talk = _talkProvider;
+  static const talk = _talkProvider;
 
-  static Future<Result<Talk, Failure>> createTalk({
-    required WidgetRef ref,
-    required String groupName,
-    required List<Contact> admins,
-    required List<Contact> members,
-  }) =>
-      Result.guard(() async {
-        final session = ref.watch(SessionProviders.session).loggedIn;
-        if (session == null) throw const Failure.loggedOut();
-
-        final selectedAccount =
-            await ref.watch(AccountProviders.selectedAccount.future);
-        if (selectedAccount == null) throw const Failure.loggedOut();
-
-        final talk = await ref
-            .watch(_messengerRepository)
-            .createTalk(
-              networkSettings: ref.watch(SettingsProviders.settings).network,
-              admins: admins,
-              members: members,
-              creator: selectedAccount,
-              session: session,
-              groupName: groupName,
-            )
-            .valueOrThrow;
-        ref.invalidate(talkIds);
-
-        return talk;
-      });
+  static final creationForm = createTalkFormProvider;
 }
