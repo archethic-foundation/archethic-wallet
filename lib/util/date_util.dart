0c301f3b84bad519c3134bb06a2f25b8d7efe507
redDwarf03 <rddwarf03@gmail.com>
2023-06-08 20:56:16 +0200

    add informations

--- lib/util/date_util.dart
+++ lib/util/date_util.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 extension DateTimeUtil on DateTime {
   DateTime get startOfDay => DateTime(
         year,
@@ -9,4 +12,34 @@ extension DateTimeUtil on DateTime {
 extension DurationUtil on Duration {
   Duration min(Duration other) => other < this ? other : this;
   Duration max(Duration other) => other > this ? other : this;
+}
+
+extension FormatDateTimeDependingDays on DateTime {
+  String format(BuildContext context) {
+    if (difference(DateTime.now()).inDays < 1) {
+      return DateFormat().add_Hms().format(this);
+    } else {
+      if (difference(DateTime.now()).inDays < 30) {
+        return DateFormat.MEd(
+          Localizations.localeOf(context).languageCode,
+        ).add_Hms().format(
+              this,
+            );
+      } else {
+        if (difference(DateTime.now()).inDays < 365) {
+          return DateFormat.MMMEd(
+            Localizations.localeOf(context).languageCode,
+          ).add_Hms().format(
+                this,
+              );
+        } else {
+          return DateFormat.yMMMEd(
+            Localizations.localeOf(context).languageCode,
+          ).add_Hms().format(
+                this,
+              );
+        }
+      }
+    }
+  }
 }
