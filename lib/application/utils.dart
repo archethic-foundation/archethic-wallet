e27d470da158af099658523101ac7aa13efe3c78
Chralu <contact@charly-caulet.net>
2023-09-06 22:01:36 +0200

    :rotating_light: update Provider streamWithCurrentValue implementation.

--- lib/application/utils.dart
+++ lib/application/utils.dart
@@ -38,10 +38,28 @@ extension WidgetRefExt on WidgetRef {
   /// Creates a Stream containing the Provider values.
   /// Stream is initialized with the last Provider value.
   /// Then, every Provider update is added to the stream.
+  /// When Stream is not listened anymore, Provider is not watched anymore.
   Stream<T> streamWithCurrentValue<T>(
     AutoDisposeStreamProvider<T> provider,
   ) async* {
     yield await read(provider.future);
-    yield* watch(provider.stream);
+
+    ProviderSubscription<AsyncValue<T>>? subscription;
+    final streamController = StreamController<T>(
+      onCancel: () {
+        subscription?.close();
+      },
+    );
+
+    subscription = listenManual(
+      provider,
+      (previous, next) {
+        final value = next.valueOrNull;
+        if (value == null) return;
+        streamController.add(value);
+      },
+    );
+
+    yield* streamController.stream;
   }
 }
