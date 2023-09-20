bee55da31be992e81a5eae6923ca56129f8d3ae5
Guillaume Daguin <81367366+gdaguin@users.noreply.github.com>
2023-09-20 12:05:23 +0200

    More logic into the provider

--- lib/ui/views/main/bloc/providers.dart
+++ lib/ui/views/main/bloc/providers.dart
@@ -1,17 +1,32 @@
+import 'package:aewallet/domain/repositories/features_flags.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final mainTabControllerProvider =
-    StateNotifierProvider<TabControllerNotifier, TabController?>((ref) {
+    StateNotifierProvider.autoDispose<TabControllerNotifier, TabController?>(
+        (ref) {
   return TabControllerNotifier();
 });
 
 class TabControllerNotifier extends StateNotifier<TabController?> {
-  TabControllerNotifier() : super(null);
+  TabControllerNotifier() : super(null) {
+    if (FeatureFlags.messagingActive) {
+      tabCount++;
+    }
+  }
 
-  TabController? get provider => state;
+  int tabCount = 4;
 
-  set provider(TabController? tabController) {
-    state = tabController;
+  void initState(TickerProvider tickerProvider) {
+    state = TabController(
+      length: tabCount,
+      vsync: tickerProvider,
+    );
+  }
+
+  @override
+  void dispose() {
+    state?.dispose();
+    super.dispose();
   }
 }
