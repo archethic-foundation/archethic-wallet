67ef7c3dd3188076d26dcaec43b1f868cfb7894c
redDwarf03 <sylvain@archethic.net>
2023-11-07 07:31:37 +0100

    ⚡️ Remove stars animation

--- lib/ui/views/main/keychain_tab.dart
+++ lib/ui/views/main/keychain_tab.dart
@@ -1,13 +1,11 @@
 import 'package:aewallet/application/account/providers.dart';
 import 'package:aewallet/application/settings/settings.dart';
 import 'package:aewallet/ui/themes/archethic_theme.dart';
-import 'package:aewallet/ui/themes/archethic_theme_base.dart';
 import 'package:aewallet/ui/views/accounts/layouts/account_list.dart';
 import 'package:aewallet/ui/views/accounts/layouts/components/add_account_button.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lit_starfield/view.dart';
 
 class KeychainTab extends ConsumerWidget {
   const KeychainTab({super.key});
@@ -28,77 +26,49 @@ class KeychainTab extends ConsumerWidget {
           opacity: 0.7,
         ),
       ),
-      child: Stack(
-        children: [
-          Opacity(
-            opacity: 0.3,
-            child: LitStarfieldContainer(
-              velocity: 0.2,
-              number: MediaQuery.of(context).size.width >= 580 ? 600 : 300,
-              starColor: ArchethicThemeBase.neutral0,
-              scale: 3,
-              backgroundDecoration: const BoxDecoration(
-                color: Colors.transparent,
-              ),
-            ),
+      child: SingleChildScrollView(
+        child: ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(
+            dragDevices: {
+              PointerDeviceKind.touch,
+              PointerDeviceKind.mouse,
+              PointerDeviceKind.trackpad,
+            },
           ),
-          Opacity(
-            opacity: 0.3,
-            child: LitStarfieldContainer(
-              velocity: 0.2,
-              number: MediaQuery.of(context).size.width >= 580 ? 300 : 100,
-              scale: 6,
-              starColor: ArchethicThemeBase.blue500,
-              backgroundDecoration: const BoxDecoration(
-                color: Colors.transparent,
-              ),
-            ),
-          ),
-          SingleChildScrollView(
-            child: ScrollConfiguration(
-              behavior: ScrollConfiguration.of(context).copyWith(
-                dragDevices: {
-                  PointerDeviceKind.touch,
-                  PointerDeviceKind.mouse,
-                  PointerDeviceKind.trackpad,
-                },
-              ),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height,
-                child: Stack(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 15,
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      AccountsListWidget(
+                        currencyName: settings.currency.name,
+                        accountsList: accountsList,
                       ),
-                      child: Column(
-                        children: <Widget>[
-                          AccountsListWidget(
-                            currencyName: settings.currency.name,
-                            accountsList: accountsList,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                          bottom: MediaQuery.of(context).padding.bottom,
-                        ),
-                        child: const Row(
-                          children: [
-                            AddAccountButton(),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).padding.bottom,
+                    ),
+                    child: const Row(
+                      children: [
+                        AddAccountButton(),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
