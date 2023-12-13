b49c9de7f1a8dad4b51a453d9e40ca17827ccefc
Chralu <contact@charly-caulet.net>
2023-12-13 22:59:31 +0100

    🐛 Pin and Password screen stacked on rootNavigator.

--- lib/router.dart
+++ lib/router.dart
@@ -144,30 +144,33 @@ class RoutesPath {
     );
   }
 
-  final _authenticationRoutes = [
-    GoRoute(
-      path: PasswordScreen.routerPage,
-      builder: (context, state) {
-        final args = state.extra! as Map<String, dynamic>;
-        return PasswordScreen(
-          canNavigateBack: args['canNavigateBack']! as bool,
-        );
-      },
-    ),
-    GoRoute(
-      path: PinScreen.routerPage,
-      builder: (context, state) {
-        final args = state.extra! as Map<String, dynamic>;
-        return PinScreen(
-          PinOverlayType.values.byName(args['type']! as String),
-          canNavigateBack: args['canNavigateBack'] == null ||
-              args['canNavigateBack']! as bool,
-          description:
-              args['description'] == null ? '' : args['description']! as String,
-        );
-      },
-    ),
-  ];
+  List<GoRoute> get _authenticationRoutes => [
+        GoRoute(
+          path: PasswordScreen.routerPage,
+          parentNavigatorKey: rootNavigatorKey,
+          builder: (context, state) {
+            final args = state.extra! as Map<String, dynamic>;
+            return PasswordScreen(
+              canNavigateBack: args['canNavigateBack']! as bool,
+            );
+          },
+        ),
+        GoRoute(
+          path: PinScreen.routerPage,
+          parentNavigatorKey: rootNavigatorKey,
+          builder: (context, state) {
+            final args = state.extra! as Map<String, dynamic>;
+            return PinScreen(
+              PinOverlayType.values.byName(args['type']! as String),
+              canNavigateBack: args['canNavigateBack'] == null ||
+                  args['canNavigateBack']! as bool,
+              description: args['description'] == null
+                  ? ''
+                  : args['description']! as String,
+            );
+          },
+        ),
+      ];
 
   final _introductionRoutes = [
     GoRoute(
