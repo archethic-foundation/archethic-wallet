db8fac0e82d6a35c930903638926c99c547b4b44
redDwarf03 <sylvain@archethic.net>
2024-01-05 17:03:02 +0100

    🐛 Manage recipient property

--- lib/infrastructure/rpc/send_transaction/command_handler.dart
+++ lib/infrastructure/rpc/send_transaction/command_handler.dart
@@ -9,16 +9,34 @@ class RPCSendTransactionCommandHandler extends RPCCommandHandler<
   RPCSendTransactionCommandHandler() : super();
 
   @override
-  RPCCommand<RPCSendTransactionCommandData> commandToModel(RPCRequestDTO dto) =>
-      RPCCommand(
-        origin: dto.origin.toModel(),
-        data: RPCSendTransactionCommandData(
-          data: archethic.Data.fromJson(dto.payload['data']),
-          type: dto.payload['type'],
-          version: dto.version,
-          generateEncryptedSeedSC: dto.payload['generateEncryptedSeedSC'],
-        ),
-      );
+  RPCCommand<RPCSendTransactionCommandData> commandToModel(RPCRequestDTO dto) {
+    if ((dto.payload['data'] as Map<String, dynamic>)
+        .containsKey('recipients')) {
+      if (dto.payload['data']['recipients'] is List<String>) {
+        final _newRecipients = <archethic.Recipient>[];
+        for (final recipientAddress
+            in dto.payload['data']['recipients'] as List<String>) {
+          final _recipient = archethic.Recipient(address: recipientAddress);
+          _newRecipients.add(_recipient);
+        }
+        dto.payload['data']['actionRecipients'] = _newRecipients;
+      } else {
+        dto.payload['data']['actionRecipients'] =
+            dto.payload['data']['recipients'];
+      }
+      (dto.payload['data'] as Map<String, dynamic>).remove('recipients');
+    }
+
+    return RPCCommand(
+      origin: dto.origin.toModel(),
+      data: RPCSendTransactionCommandData(
+        data: archethic.Data.fromJson(dto.payload['data']),
+        type: dto.payload['type'],
+        version: dto.version,
+        generateEncryptedSeedSC: dto.payload['generateEncryptedSeedSC'],
+      ),
+    );
+  }
 
   @override
   Map<String, dynamic> resultFromModel(
