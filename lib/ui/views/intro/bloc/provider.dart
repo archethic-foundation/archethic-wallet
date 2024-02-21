00742621e6a2a1a403e15b18bac9299e489ace26
redDwarf03 <sylvain@archethic.net>
2024-02-21 18:21:40 +0100

    Display google and discord options only on ios / android

--- lib/ui/views/intro/bloc/provider.dart
+++ lib/ui/views/intro/bloc/provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:aewallet/model/authentication_method.dart';
 import 'package:aewallet/util/biometrics_util.dart';
 import 'package:aewallet/util/get_it_instance.dart';
@@ -23,6 +25,11 @@ class IntroProviders {
           accessModes.add(AuthMethod.biometrics);
         }
         accessModes.add(AuthMethod.yubikeyWithYubicloud);
+        if (Platform.isAndroid || Platform.isIOS) {
+          accessModes
+            ..add(AuthMethod.discord)
+            ..add(AuthMethod.google);
+        }
       }
       return accessModes;
     },
