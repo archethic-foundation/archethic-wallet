7ee0441a74eb0a5730b348c7e211d7b3b9195a05
redDwarf03 <sylvain@archethic.net>
2024-03-11 17:56:15 +0100

    🐛 Fix transparent widget

--- lib/ui/views/nft_creation/layouts/components/card_category.dart
+++ lib/ui/views/nft_creation/layouts/components/card_category.dart
@@ -1,19 +1,35 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 
 import 'dart:ui';
+
+import 'package:aewallet/application/settings/settings.dart';
 import 'package:aewallet/ui/themes/styles.dart';
+import 'package:aewallet/ui/views/nft/layouts/nft_list_per_category.dart';
+import 'package:aewallet/util/get_it_instance.dart';
+import 'package:aewallet/util/haptic_util.dart';
 import 'package:archethic_dapp_framework_flutter/archethic_dapp_framework_flutter.dart'
     as aedappfm;
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
+import 'package:go_router/go_router.dart';
 
 class CardCategory extends ConsumerWidget {
-  const CardCategory({super.key, required this.categoryName});
+  const CardCategory({
+    super.key,
+    required this.categoryName,
+    required this.index,
+    required this.id,
+  });
 
   final String categoryName;
+  final int index;
+  final int id;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final preferences = ref.watch(SettingsProviders.settings);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(16),
       child: BackdropFilter(
@@ -26,8 +42,21 @@ class CardCategory extends ConsumerWidget {
               color: aedappfm.AppThemeBase.sheetBorder,
             ),
           ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(16),
+          child: InkWell(
+            customBorder: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            key: Key('nftCategory$index'),
+            onTap: () {
+              sl.get<HapticUtil>().feedback(
+                    FeedbackType.light,
+                    preferences.activeVibrations,
+                  );
+              context.go(
+                NFTListPerCategory.routerPage,
+                extra: id,
+              );
+            },
             child: Container(
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.width / 4,
