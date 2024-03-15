2f16e47f04428eb27f952235aa82cceb451caea1
redDwarf03 <sylvain@archethic.net>
2024-03-15 12:09:08 +0100

    🐛 Fix migration tool

--- lib/application/migrations/512.dart
+++ lib/application/migrations/512.dart
@@ -2,18 +2,12 @@
 
 part of 'migration_manager.dart';
 
-final migration_492 = LocalDataMigration(
-  minAppVersion: 492,
+final migration_512 = LocalDataMigration(
+  minAppVersion: 512,
   run: (ref) async {
     // We need to get the genesis address for contact's list
     // https://github.com/archethic-foundation/archethic-wallet/issues/887
-    if (ref.read(SessionProviders.session).isLoggedOut) {
-      log(
-        'Skipping migration 492 process : user logged out.',
-        name: logName,
-      );
-      return;
-    }
+
     final contacts = await ref.read(
       ContactProviders.fetchContacts().future,
     );
