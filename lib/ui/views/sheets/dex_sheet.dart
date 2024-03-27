ee736728d36bc5b82772c2194522c032e342a18f
Chralu <contact@charly-caulet.net>
2024-03-27 15:18:49 +0100

    ✨ Show error message when AWCWebview not supported by OS.

--- lib/ui/views/sheets/dex_sheet.dart
+++ lib/ui/views/sheets/dex_sheet.dart
@@ -1,42 +1,99 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 
-import 'dart:io';
-
+import 'package:aewallet/application/settings/settings.dart';
 import 'package:aewallet/infrastructure/rpc/awc_webview.dart';
 import 'package:aewallet/ui/themes/archethic_theme.dart';
+import 'package:aewallet/ui/themes/styles.dart';
+import 'package:aewallet/ui/util/dimens.dart';
 import 'package:aewallet/ui/views/main/components/sheet_appbar.dart';
-import 'package:flutter/foundation.dart';
+import 'package:aewallet/ui/widgets/components/app_button_tiny.dart';
+import 'package:aewallet/ui/widgets/components/loading_list_header.dart';
+import 'package:aewallet/ui/widgets/components/sheet_header.dart';
+import 'package:aewallet/util/get_it_instance.dart';
+import 'package:aewallet/util/haptic_util.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
 import 'package:go_router/go_router.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
-// TODO(redDwarf03): WebView POC - To remove
-class DEXSheet extends ConsumerStatefulWidget {
+class UnavailableFeatureWarning extends ConsumerWidget {
+  const UnavailableFeatureWarning({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final preferences = ref.watch(SettingsProviders.settings);
+    final localizations = AppLocalizations.of(context)!;
+    return SafeArea(
+      minimum: EdgeInsets.only(
+        bottom: MediaQuery.of(context).size.height * 0.035,
+      ),
+      child: Column(
+        children: <Widget>[
+          SheetHeader(title: localizations.information),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: [
+                      const Icon(Symbols.warning, color: Colors.red),
+                      const SizedBox(width: 10),
+                      Text(
+                        localizations.webChannelIncompatibilityWarning,
+                        style: ArchethicThemeStyles.textStyleSize14W600Primary,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    localizations.webChannelIncompatibilityWarningDesc,
+                    style: ArchethicThemeStyles.textStyleSize12W100Primary,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              AppButtonTiny(
+                AppButtonTinyType.primary,
+                localizations.understandButton,
+                Dimens.buttonBottomDimens,
+                key: const Key('Understand'),
+                onPressed: () async {
+                  sl.get<HapticUtil>().feedback(
+                        FeedbackType.light,
+                        preferences.activeVibrations,
+                      );
+                  context.pop();
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DEXSheet extends ConsumerWidget {
   const DEXSheet({
     super.key,
   });
 
   static bool get isAvailable => AWCWebview.isAvailable;
-
   static const String routerPage = '/dex';
 
   @override
-  ConsumerState<DEXSheet> createState() => DEXSheetState();
-}
-
-class DEXSheetState extends ConsumerState<DEXSheet> {
-  @override
-  void initState() {
-    if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
-      InAppWebViewController.setWebContentsDebuggingEnabled(true);
-    }
-
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: SheetAppBar(
         title: 'Dex',
@@ -49,8 +106,20 @@ class DEXSheetState extends ConsumerState<DEXSheet> {
         ),
       ),
       body: SafeArea(
-        child: AWCWebview(
-          uri: Uri.parse('https://dex.testnet.archethic.net'),
+        child: FutureBuilder<bool>(
+          future: AWCWebview.isAWCSupported,
+          builder: (context, snapshot) {
+            final isAWCSupported = snapshot.data;
+            if (isAWCSupported == null) {
+              return const Center(child: LoadingListHeader());
+            }
+
+            if (!isAWCSupported) return const UnavailableFeatureWarning();
+
+            return AWCWebview(
+              uri: Uri.parse('https://dex.testnet.archethic.net'),
+            );
+          },
         ),
       ),
     );
