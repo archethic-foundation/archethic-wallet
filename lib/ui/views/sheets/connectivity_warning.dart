d4acc78db4e429e0841bf462e7a60a1e3423a56e
redDwarf03 <sylvain@archethic.net>
2024-03-28 16:49:40 +0100

    🐛 Fix widget display

--- lib/ui/views/sheets/connectivity_warning.dart
+++ lib/ui/views/sheets/connectivity_warning.dart
@@ -3,8 +3,10 @@
 import 'package:aewallet/application/settings/settings.dart';
 import 'package:aewallet/ui/themes/styles.dart';
 import 'package:aewallet/ui/util/dimens.dart';
+import 'package:aewallet/ui/views/main/components/sheet_appbar.dart';
 import 'package:aewallet/ui/widgets/components/app_button_tiny.dart';
-import 'package:aewallet/ui/widgets/components/sheet_header.dart';
+import 'package:aewallet/ui/widgets/components/sheet_skeleton.dart';
+import 'package:aewallet/ui/widgets/components/sheet_skeleton_interface.dart';
 import 'package:aewallet/util/get_it_instance.dart';
 import 'package:aewallet/util/haptic_util.dart';
 import 'package:flutter/material.dart';
@@ -14,70 +16,73 @@ import 'package:flutter_vibrate/flutter_vibrate.dart';
 import 'package:go_router/go_router.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
-class ConnectivityWarning extends ConsumerWidget {
+class ConnectivityWarning extends ConsumerWidget
+    implements SheetSkeletonInterface {
   const ConnectivityWarning({super.key});
 
   static const routerPage = '/connectivity_warning';
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    return SheetSkeleton(
+      appBar: getAppBar(context, ref),
+      floatingActionButton: getFloatingActionButton(context, ref),
+      sheetContent: getSheetContent(context, ref),
+    );
+  }
+
+  @override
+  Widget getFloatingActionButton(BuildContext context, WidgetRef ref) {
     final preferences = ref.watch(SettingsProviders.settings);
+    return Row(
+      children: <Widget>[
+        AppButtonTiny(
+          AppButtonTinyType.primary,
+          AppLocalizations.of(context)!.understandButton,
+          Dimens.buttonBottomDimens,
+          key: const Key('Understand'),
+          onPressed: () async {
+            sl.get<HapticUtil>().feedback(
+                  FeedbackType.light,
+                  preferences.activeVibrations,
+                );
+            context.pop();
+          },
+        ),
+      ],
+    );
+  }
+
+  @override
+  PreferredSizeWidget getAppBar(BuildContext context, WidgetRef ref) {
+    return SheetAppBar(
+      title: AppLocalizations.of(context)!.information,
+    );
+  }
+
+  @override
+  Widget getSheetContent(BuildContext context, WidgetRef ref) {
     final localizations = AppLocalizations.of(context)!;
-    return SafeArea(
-      minimum: EdgeInsets.only(
-        bottom: MediaQuery.of(context).size.height * 0.035,
-      ),
-      child: Column(
-        children: <Widget>[
-          SheetHeader(title: localizations.information),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: [
-                      const Icon(Symbols.warning, color: Colors.red),
-                      const SizedBox(width: 10),
-                      Text(
-                        localizations.connectivityWarningHeader,
-                        style: ArchethicThemeStyles.textStyleSize14W600Primary,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    localizations.connectivityWarningDesc,
-                    style: ArchethicThemeStyles.textStyleSize12W100Primary,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                ],
-              ),
+    return Column(
+      children: <Widget>[
+        Row(
+          children: [
+            const Icon(Symbols.warning, color: Colors.red),
+            const SizedBox(width: 10),
+            Text(
+              localizations.connectivityWarningHeader,
+              style: ArchethicThemeStyles.textStyleSize14W600Primary,
             ),
-          ),
-          Row(
-            children: <Widget>[
-              AppButtonTiny(
-                AppButtonTinyType.primary,
-                localizations.understandButton,
-                Dimens.buttonBottomDimens,
-                key: const Key('Understand'),
-                onPressed: () async {
-                  sl.get<HapticUtil>().feedback(
-                        FeedbackType.light,
-                        preferences.activeVibrations,
-                      );
-                  context.pop();
-                },
-              ),
-            ],
-          ),
-        ],
-      ),
+          ],
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Text(
+          localizations.connectivityWarningDesc,
+          style: ArchethicThemeStyles.textStyleSize12W100Primary,
+        ),
+      ],
     );
   }
 }
