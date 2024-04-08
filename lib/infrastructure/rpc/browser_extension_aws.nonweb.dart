8e08082eca49e6941e134c553386206ee99e1f18
Chralu <contact@charly-caulet.net>
2024-04-08 12:26:23 +0200

    🚧 Restore non web compatibility

---
+++ lib/infrastructure/rpc/browser_extension_aws.nonweb.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+class BrowserExtensionAWS {
+  BrowserExtensionAWS() {
+    throw UnimplementedError(
+      'Browser extension is not supported on the desktop platform.',
+    );
+  }
+
+  static bool get isPlatformCompatible => false;
+
+  Future<void> run() async {}
+}
