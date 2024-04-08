0f17e8e4841f50f091b9bf63d69e38d0242725bf
Chralu <contact@charly-caulet.net>
2024-04-08 20:35:11 +0200

    🚧 various fixes.

--- web chrome extension/src/background.js
+++ web chrome extension/src/background.js
@@ -1,25 +1,62 @@
 class AWS {
-    #extensionPageConnectionResolver = new ExtensionPageConnectionResolver()
     run() {
+        const extensionPortMap = new ExtensionPortsMap()
         chrome.runtime.onConnectExternal.addListener(async (webPagePort) => {
-            console.log('Webpage connected')
+            console.log(`Webpage connected `)
+
+            const transmitFromExtensionToWebPage = (message) => {
+                console.log(`Extension response received ${message}`)
+                webPagePort.postMessage(message)
+                console.log(`Extension response transmitted`)
+            }
+
             webPagePort.onMessage.addListener((message) => {
-                console.log('Message received')
-                this.#extensionPageConnectionResolver.waitForConnection((isNew, extensionPagePort) => {
-                    if (isNew) {
-                        extensionPagePort.onMessage.addListener((message) => {
-                            console.log(`Response received ${message}`)
-                            webPagePort.postMessage(message)
+                console.log(`Webpage message received : ${message}`)
+                extensionPortMap.getPort(
+                    webPagePort.sender.origin,
+                    (extensionPagePort) => {
+                        extensionPagePort.onMessage.addListener(transmitFromExtensionToWebPage)
+                        webPagePort.onDisconnect.addListener((_, __) => {
+                            console.log(`Webpage disconnected`)
+                            extensionPagePort.onMessage.removeListener(transmitFromExtensionToWebPage);
                         })
-                    }
-                    extensionPagePort.postMessage(message)
-                });
+                        extensionPagePort.postMessage(message)
+                    });
             })
+
         })
     }
 }
 
-class ExtensionPageConnectionResolver {
+class ExtensionPortsMap {
+    extensionPortResolvers = {}
+
+    getPort(id, callback) {
+        this.#getExtensionPortResolver(id).waitForConnection((port) => {
+            callback(port);
+        })
+    }
+
+    #releaseExtensionPortResolver(id) {
+        this.extensionPortResolvers[id] = undefined
+    }
+
+    #getExtensionPortResolver(id) {
+        const extensionPortResolver = this.extensionPortResolvers[id]
+        if (extensionPortResolver !== undefined) return extensionPortResolver
+
+        const newExtensionPortResolver = new ExtensionPortResolver()
+        newExtensionPortResolver.onDisconnect = () => {
+            console.log(`Extension port released for ${id}`)
+            this.#releaseExtensionPortResolver(id)
+        }
+        console.log(`Extension port created for ${id}`)
+        this.extensionPortResolvers[id] = newExtensionPortResolver
+        return newExtensionPortResolver
+    }
+}
+
+class ExtensionPortResolver {
     #extensionPopupPort = null
 
     #callbacksWaitingForConnection = []
@@ -32,7 +69,7 @@ class ExtensionPageConnectionResolver {
 
             for (const callback of this.#callbacksWaitingForConnection) {
                 console.log('Resolving callback')
-                callback(true, this.#extensionPopupPort)
+                callback(this.#extensionPopupPort)
             }
             this.#callbacksWaitingForConnection = []
             console.log('Callbacks resolving done')
@@ -40,21 +77,24 @@ class ExtensionPageConnectionResolver {
             this.#extensionPopupPort.onDisconnect.addListener(() => {
                 console.log('Extension popup disconnected')
                 this.#extensionPopupPort = null
+                this.onDisconnect()
             })
             console.log('Extension popup ready')
             return
         })
     }
 
+    onDisconnect = () => { }
+
     /**
      * 
-     * @param {Function(isNew: boolean, port: Port)} callback 
+     * @param {Function(port: Port)} callback 
      * @returns 
      */
     waitForConnection(callback) {
         if (this.#extensionPopupPort != null) {
             console.log('Extension popup ready')
-            callback(false, this.#extensionPopupPort)
+            callback(this.#extensionPopupPort)
             return
         }
         this.#callbacksWaitingForConnection.push(callback)
--- web chrome extension/src/content.js
+++ web chrome extension/src/content.js
@@ -18,7 +18,7 @@
 
 var s = document.createElement('script');
 s.dataset.params = JSON.stringify({ extensionId: chrome.runtime.id })
-s.src = chrome.runtime.getURL('awc.js');
+s.src = chrome.runtime.getURL('archethic.js');
 s.onload = function () { this.remove(); };
 // see also "Dynamic values in the injected code" section in this answer
 (document.head || document.documentElement).appendChild(s);
