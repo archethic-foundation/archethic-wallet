12766d60d2ddd3040b74e62157e8bf306faa8456
Chralu <contact@charly-caulet.net>
2024-04-25 13:13:41 +0200

    🚧 Add password storage encryption.

---
+++ test/secure_storage_test.dart
@@ -0,0 +1,195 @@
+import 'dart:typed_data';
+
+import 'package:aewallet/infrastructure/datasources/secured_datasource_mixin.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pointycastle/pointycastle.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  group(
+    'Vault',
+    () {
+      setUp(() async {
+        FlutterSecureStorage.setMockInitialValues({});
+      });
+
+      group(
+        'Vault key (without password)',
+        () {
+          test(
+            'Should generate & read Vault encryption key with right password',
+            () async {
+              const secureStorage = FlutterSecureStorage();
+              final expectedEncryptionKey =
+                  await Hive.generateAndStoreSecureKey(
+                secureStorage,
+              );
+
+              final readEncryptionKey = await Hive.readSecureKey(
+                secureStorage,
+              );
+
+              expect(readEncryptionKey, expectedEncryptionKey);
+            },
+          );
+        },
+      );
+
+      group(
+        'Vault key with password creation',
+        () {
+          test(
+            'Should generate & read Vault encryption key with right password',
+            () async {
+              const secureStorage = FlutterSecureStorage();
+              final expectedEncryptionKey =
+                  await Hive.generateAndStoreEncryptedSecureKey(
+                secureStorage,
+                'password_ok',
+              );
+
+              final readEncryptionKey = await Hive.readEncryptedSecureKey(
+                secureStorage,
+                'password_ok',
+              );
+
+              expect(readEncryptionKey, expectedEncryptionKey);
+            },
+          );
+
+          test(
+            'Should throw InvalidCipherTextException when reading Vault encryption key with wrong password',
+            () async {
+              const secureStorage = FlutterSecureStorage();
+              await Hive.generateAndStoreEncryptedSecureKey(
+                secureStorage,
+                'password_ok',
+              );
+
+              expect(
+                () async => Hive.readEncryptedSecureKey(
+                  secureStorage,
+                  'wrong_password',
+                ),
+                throwsA(isA<InvalidCipherTextException>()),
+              );
+            },
+          );
+        },
+      );
+      group(
+        'Vault key with password update',
+        () {
+          test(
+            'Should update Vault encryption key password',
+            () async {
+              const secureStorage = FlutterSecureStorage();
+              final expectedEncryptionKey =
+                  await Hive.generateAndStoreEncryptedSecureKey(
+                secureStorage,
+                'previous_password',
+              );
+
+              await Hive.updateAndStoreEncryptedSecureKey(
+                secureStorage,
+                'previous_password',
+                'new_password',
+              );
+
+              final readEncryptionKey = await Hive.readEncryptedSecureKey(
+                secureStorage,
+                'new_password',
+              );
+
+              expect(readEncryptionKey, expectedEncryptionKey);
+            },
+          );
+
+          test(
+            'Should not update Vault encryption key password when never created before',
+            () async {
+              const secureStorage = FlutterSecureStorage();
+
+              final newEncryptedSecureKey =
+                  await Hive.updateAndStoreEncryptedSecureKey(
+                secureStorage,
+                'previous_password',
+                'new_password',
+              );
+              expect(
+                newEncryptedSecureKey,
+                null,
+              );
+            },
+          );
+        },
+      );
+      group(
+        'Secure key encryption key with password & salt',
+        () {
+          test(
+            'Should decrypt securedKey with right salt and password',
+            () async {
+              final securedKey = Uint8List.fromList([1, 2, 3, 4]);
+              final encryptedSecureKey = Hive.encryptSecureKey(
+                'salt_ok',
+                'password_ok',
+                securedKey,
+              );
+              final decryptedSecureKey = Hive.decryptSecureKey(
+                'salt_ok',
+                'password_ok',
+                encryptedSecureKey,
+              );
+
+              expect(decryptedSecureKey, securedKey);
+            },
+          );
+
+          test(
+            'Should return null when wrong salt and right password',
+            () async {
+              final encryptedSecureKey = Hive.encryptSecureKey(
+                'salt_ok',
+                'password_ok',
+                Uint8List.fromList([1, 2, 3, 4]),
+              );
+
+              final decryptedSecureKey = Hive.decryptSecureKey(
+                'wrong_salt',
+                'password_ok',
+                encryptedSecureKey,
+              );
+              expect(
+                decryptedSecureKey,
+                null,
+              );
+            },
+          );
+
+          test(
+            'Should return null when right salt and wrong password',
+            () async {
+              final encryptedSecuredKey = Hive.encryptSecureKey(
+                'salt_ok',
+                'password_ok',
+                Uint8List.fromList([1, 2, 3, 4]),
+              );
+              final decryptedKey = Hive.decryptSecureKey(
+                'salt_ok',
+                'wrong_password',
+                encryptedSecuredKey,
+              );
+              expect(
+                decryptedKey,
+                null,
+              );
+            },
+          );
+        },
+      );
+    },
+  );
+}
