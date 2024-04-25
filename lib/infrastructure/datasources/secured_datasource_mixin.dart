9260adf0248c2a7df837c1e3c92f7d377038e8d0
Chralu <contact@charly-caulet.net>
2024-04-25 13:13:41 +0200

    🚧 Add password storage encryption.

--- lib/infrastructure/datasources/secured_datasource_mixin.dart
+++ lib/infrastructure/datasources/secured_datasource_mixin.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:archethic_lib_dart/archethic_lib_dart.dart' as archethic;
 import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:pointycastle/export.dart';
 
 mixin SecuredHiveMixin {
-  final List<int> secureKey = Hive.generateSecureKey();
+  // final List<int> secureKey = Hive.generateSecureKey();
 
   // This doesn't have to be a singleton.
   // We just want to make sure that the box is open, before we start getting/setting objects on it
@@ -18,8 +20,7 @@ mixin SecuredHiveMixin {
       return await Hive.openBox<E>(
         name,
         encryptionCipher: kIsWeb
-            // TODO(reddwarf03): Sécuriser le web
-            ? await _prepareCipher()
+            ? await _prepareCipherWeb(password!)
             : await _prepareCipher(),
       );
     } catch (e, stack) {
@@ -42,8 +43,7 @@ mixin SecuredHiveMixin {
       return Hive.openLazyBox<E>(
         name,
         encryptionCipher: kIsWeb
-            // TODO(reddwarf03): Sécuriser le web
-            ? await _prepareCipher()
+            ? await _prepareCipherWeb(password!)
             : await _prepareCipher(),
       );
     } catch (e, stack) {
@@ -58,33 +58,182 @@ mixin SecuredHiveMixin {
 
   static Future<HiveCipher> _prepareCipher() async {
     const secureStorage = FlutterSecureStorage();
-    final encryptionKey = await _readEncryptionKey(secureStorage) ??
-        await _generateEncryptionKey(secureStorage);
+    final encryptionKey = await Hive.readSecureKey(secureStorage) ??
+        await Hive.generateAndStoreSecureKey(secureStorage);
 
     return HiveAesCipher(encryptionKey);
   }
 
-  static Future<Uint8List> _generateEncryptionKey(
+  static Future<HiveCipher> _prepareCipherWeb(String password) async {
+    const secureStorage = FlutterSecureStorage();
+
+    final encryptionKey = await Hive.readEncryptedSecureKey(
+          secureStorage,
+          password,
+        ) ??
+        await Hive.generateAndStoreEncryptedSecureKey(
+          secureStorage,
+          password,
+        );
+
+    return HiveAesCipher(encryptionKey);
+  }
+}
+
+extension HiveEncryptedSecuredKey on HiveInterface {
+  Future<Uint8List> generateAndStoreSecureKey(
     FlutterSecureStorage secureStorage,
   ) async {
-    final key = Hive.generateSecureKey();
-    final secureKey = base64UrlEncode(key);
+    final hiveKey = Hive.generateSecureKey();
+
     await secureStorage.write(
       key: 'archethic_wallet_secure_key',
-      value: secureKey,
+      value: base64UrlEncode(hiveKey.toList()),
     );
-    return Uint8List.fromList(key);
+
+    return Uint8List.fromList(hiveKey);
   }
 
-  static Future<Uint8List?> _readEncryptionKey(
+  Future<Uint8List?> readSecureKey(
     FlutterSecureStorage secureStorage,
   ) async {
-    final secureKey = await secureStorage.read(
+    final keyBase64 = await secureStorage.read(
       key: 'archethic_wallet_secure_key',
     );
-    if (secureKey == null || secureKey.isEmpty) {
+
+    if (keyBase64 == null) {
       return null;
     }
-    return base64Url.decode(secureKey);
+
+    return base64Url.decode(keyBase64);
+  }
+
+  Future<Uint8List?> updateAndStoreEncryptedSecureKey(
+    FlutterSecureStorage secureStorage,
+    String previousPassword,
+    String newPassword,
+  ) async {
+    final hiveKey = await readEncryptedSecureKey(
+      secureStorage,
+      previousPassword,
+    );
+
+    if (hiveKey == null) return null;
+
+    final salt = archethic.generateRandomSeed();
+    final encryptedKey = encryptSecureKey(
+      salt,
+      newPassword,
+      hiveKey,
+    );
+
+    await secureStorage.write(
+      key: 'archethic_wallet_encrypted_secure_key',
+      value: base64UrlEncode(encryptedKey.toList()),
+    );
+    await secureStorage.write(
+      key: 'archethic_wallet_encrypted_secure_key_salt',
+      value: base64UrlEncode(archethic.hexToUint8List(salt)),
+    );
+
+    return Uint8List.fromList(hiveKey);
+  }
+
+  Future<Uint8List> generateAndStoreEncryptedSecureKey(
+    FlutterSecureStorage secureStorage,
+    String password,
+  ) async {
+    final salt = archethic.generateRandomSeed();
+
+    final hiveKey = Hive.generateSecureKey();
+    final encryptedKey = encryptSecureKey(
+      salt,
+      password,
+      Uint8List.fromList(hiveKey),
+    );
+
+    await secureStorage.write(
+      key: 'archethic_wallet_encrypted_secure_key',
+      value: base64UrlEncode(encryptedKey.toList()),
+    );
+    await secureStorage.write(
+      key: 'archethic_wallet_encrypted_secure_key_salt',
+      value: base64UrlEncode(archethic.hexToUint8List(salt)),
+    );
+
+    return Uint8List.fromList(hiveKey);
+  }
+
+  Future<Uint8List?> readEncryptedSecureKey(
+    FlutterSecureStorage secureStorage,
+    String password,
+  ) async {
+    final encryptedKeyBase64 = await secureStorage.read(
+      key: 'archethic_wallet_encrypted_secure_key',
+    );
+    final saltBase64 = await secureStorage.read(
+      key: 'archethic_wallet_encrypted_secure_key_salt',
+    );
+
+    if (encryptedKeyBase64 == null || saltBase64 == null) {
+      return null;
+    }
+
+    final encryptedKey = base64Url.decode(encryptedKeyBase64);
+    final salt = archethic.uint8ListToHex(base64Url.decode(saltBase64));
+
+    final decryptedKey = decryptSecureKey(salt, password, encryptedKey);
+
+    return decryptedKey;
+  }
+
+  Uint8List encryptSecureKey(
+    String salt,
+    String password,
+    Uint8List clearSecuredKey,
+  ) {
+    final derivedKey = _generatePBKDFKey(password, salt);
+
+    return archethic.aesEncrypt(
+      archethic.uint8ListToHex(Uint8List.fromList(clearSecuredKey)),
+      derivedKey,
+    );
+  }
+
+  Uint8List? decryptSecureKey(
+    String salt,
+    String password,
+    Uint8List encryptedSecuredKey,
+  ) {
+    final derivedKey = _generatePBKDFKey(password, salt);
+
+    try {
+      return archethic.aesDecrypt(encryptedSecuredKey, derivedKey);
+    } on InvalidCipherTextException {
+      return null;
+    }
+  }
+
+  // method to generate encryption key using user's password.
+  static Uint8List _generatePBKDFKey(
+    String password,
+    String salt, {
+    int iterations = 10000,
+    int derivedKeyLength = 32,
+  }) {
+    final passwordBytes = utf8.encode(password);
+    final saltBytes = utf8.encode(salt);
+
+    final params = Pbkdf2Parameters(
+      Uint8List.fromList(saltBytes),
+      iterations,
+      derivedKeyLength,
+    );
+    final pbkdf2 = PBKDF2KeyDerivator(HMac(SHA256Digest(), 64));
+
+    // ignore: cascade_invocations
+    pbkdf2.init(params);
+
+    return pbkdf2.process(Uint8List.fromList(passwordBytes));
   }
 }
