ff6f5a7e8cef50dc030c9245106db015fc0a1c38
Chralu <contact@charly-caulet.net>
2024-05-20 12:04:29 +0200

    🚨 [ChromeExt] Fix lint issues.

--- web/background.d.ts
+++ web/background.d.ts
@@ -1,5 +1,5 @@
 declare function findExtensionWindowId(): Promise<number | null>;
 declare function openExtensionPopup(): Promise<void>;
-declare function extensionPopupExists(): Promise<Boolean>;
-declare function isExtensionPopupReady(): Promise<Boolean>;
+declare function extensionPopupExists(): Promise<boolean>;
+declare function isExtensionPopupReady(): Promise<boolean>;
 declare function waitForExtensionPopup(): Promise<void>;
