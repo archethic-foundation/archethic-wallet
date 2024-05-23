1a946a67daf2f4a914d7fae68e545a014c460e4b
Chralu <contact@charly-caulet.net>
2024-05-23 14:29:53 +0200

    Improve Hive.deleteBox workaround to actually delete boxes themselves.
    Until now we were only emptying boxes.

--- lib/infrastructure/datasources/hive.extension.dart
+++ lib/infrastructure/datasources/hive.extension.dart
@@ -1,14 +1,19 @@
 import 'package:hive/hive.dart';
 
 extension HiveClearExtension on HiveInterface {
-  /// Workaround to [Hive.deleteBoxFromDisk] issues on web
+  /// Workaround to [HiveInterface.deleteBoxFromDisk] issues on web
   /// https://github.com/isar/hive/issues/344
   Future<void> deleteBox<E>(String boxName) async {
-    final box = Hive.isBoxOpen(boxName)
-        ? Hive.box<E>(boxName)
-        : await Hive.openLazyBox<E>(boxName);
-    await box.clear();
-    await box.compact();
-    await box.close();
+    if (Hive.isBoxOpen(boxName)) {
+      await Hive.box<E>(boxName).close();
+    }
+    await Hive.deleteBoxFromDisk(boxName);
+  }
+
+  Future<void> deleteLazyBox<E>(String boxName) async {
+    if (Hive.isBoxOpen(boxName)) {
+      await Hive.lazyBox<E>(boxName).close();
+    }
+    await Hive.deleteBoxFromDisk(boxName);
   }
 }
