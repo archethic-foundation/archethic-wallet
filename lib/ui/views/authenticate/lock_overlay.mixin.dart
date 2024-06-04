d9df8beede0b802572547b3a5f1054fa7452d048
Chralu <contact@charly-caulet.net>
2024-06-04 16:04:59 +0200

    🐛 User inputs are not caught by underlying screen when Mask hides
    Fixes https://github.com/archethic-foundation/archethic-wallet/issues/942

--- lib/ui/views/authenticate/lock_overlay.mixin.dart
+++ lib/ui/views/authenticate/lock_overlay.mixin.dart
@@ -2,40 +2,6 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
-class LockOverlay {
-  LockOverlay(this.child);
-
-  final Widget child;
-
-  OverlayEntry? _overlayEntry;
-  static const _logName = 'LockOverlay';
-
-  void show(BuildContext context) {
-    log('Show', name: _logName);
-    if (_overlayEntry != null) {
-      log('... already visible. Abort', name: _logName);
-      return;
-    }
-
-    _overlayEntry = OverlayEntry(
-      builder: (context) => child,
-    );
-    Overlay.of(context).insert(_overlayEntry!);
-  }
-
-  void hide() {
-    log('Hide', name: _logName);
-    if (_overlayEntry == null) {
-      log('... not visible. Abort', name: _logName);
-      return;
-    }
-
-    _overlayEntry?.remove();
-    _overlayEntry?.dispose();
-    _overlayEntry = null;
-  }
-}
-
 mixin LockOverlayMixin {
   Widget get child;
 
@@ -62,8 +28,16 @@ mixin LockOverlayMixin {
       return;
     }
 
-    _overlayEntry?.remove();
-    _overlayEntry?.dispose();
-    _overlayEntry = null;
+    /// That delay prevent user inputs to be caught by underlying
+    /// screen during Lock overlay hiding.
+    /// https://github.com/archethic-foundation/archethic-wallet/issues/942
+    Future.delayed(
+      const Duration(milliseconds: 50),
+      () {
+        _overlayEntry?.remove();
+        _overlayEntry?.dispose();
+        _overlayEntry = null;
+      },
+    );
   }
 }
