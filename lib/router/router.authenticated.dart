14568fe79d901527d34312821c9c848df3b90198
redDwarf03 <sylvain@archethic.net>
2024-08-28 15:41:54 +0200

    chore: Add aeSwap links

--- lib/router/router.authenticated.dart
+++ lib/router/router.authenticated.dart
@@ -114,17 +114,6 @@ final _authenticatedRoutes = [
     ),
   ),
   GoRoute(
-    path: DEXSheet.routerPage,
-    pageBuilder: (context, state) => CustomTransitionPage<void>(
-      transitionDuration: Duration.zero,
-      reverseTransitionDuration: Duration.zero,
-      key: state.pageKey,
-      child: const DEXSheet(),
-      transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-          FadeTransition(opacity: animation, child: child),
-    ),
-  ),
-  GoRoute(
     path: ContactDetail.routerPage,
     pageBuilder: (context, state) => CustomTransitionPage<void>(
       transitionDuration: Duration.zero,
