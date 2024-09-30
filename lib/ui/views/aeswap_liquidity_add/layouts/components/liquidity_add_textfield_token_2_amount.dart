a687433025536e9e440c40959c5e92cfafb32a4b
redDwarf03 <sylvain@archethic.net>
2024-09-30 10:55:41 +0200

    fix: :bug: Fix CircularProgressIndicator display

--- lib/ui/views/aeswap_liquidity_add/layouts/components/liquidity_add_textfield_token_2_amount.dart
+++ lib/ui/views/aeswap_liquidity_add/layouts/components/liquidity_add_textfield_token_2_amount.dart
@@ -106,19 +106,20 @@ class _LiquidityAddToken2AmountState
                           ),
                           child: liquidityAdd.calculateToken2
                               ? const SizedBox(
-                                  width: 10,
                                   height: 48,
-                                  child: Padding(
-                                    padding: EdgeInsets.only(
-                                      left: 317,
-                                      right: 10,
-                                      top: 15,
-                                      bottom: 15,
-                                    ),
-                                    child: CircularProgressIndicator(
-                                      strokeWidth: 1,
-                                      color: Colors.white,
-                                    ),
+                                  child: Row(
+                                    children: [
+                                      Spacer(),
+                                      SizedBox(
+                                        width: 20,
+                                        height: 20,
+                                        child: CircularProgressIndicator(
+                                          strokeWidth: 1,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      SizedBox(width: 10),
+                                    ],
                                   ),
                                 )
                               : TextField(
