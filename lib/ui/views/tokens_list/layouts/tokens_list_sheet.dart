ab563b3923ba276c07a9bc8a7bfa96213ae632c7
redDwarf03 <sylvain@archethic.net>
2024-10-08 23:30:42 +0200

    chore: :fire: Disable token fungible creation feature

--- lib/ui/views/tokens_list/layouts/tokens_list_sheet.dart
+++ lib/ui/views/tokens_list/layouts/tokens_list_sheet.dart
@@ -1,5 +1,6 @@
 /// SPDX-License-Identifier: AGPL-3.0-or-later
 import 'package:aewallet/application/settings/settings.dart';
+import 'package:aewallet/domain/repositories/features_flags.dart';
 import 'package:aewallet/ui/themes/archethic_theme.dart';
 import 'package:aewallet/ui/themes/styles.dart';
 import 'package:aewallet/ui/util/formatters.dart';
@@ -51,55 +52,49 @@ class TokensListState extends ConsumerState<TokensList>
             Expanded(
               child: Align(
                 alignment: Alignment.topCenter,
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    right: 15,
-                  ),
-                  child: TextFormField(
-                    textAlignVertical: TextAlignVertical.center,
-                    decoration: InputDecoration(
-                      contentPadding: EdgeInsets.zero,
-                      prefixIcon: Icon(
-                        Symbols.search,
-                        color: ArchethicTheme.text,
-                        size: 18,
-                        weight: IconSize.weightM,
-                        opticalSize: IconSize.opticalSizeM,
-                        grade: IconSize.gradeM,
-                      ),
-                      suffixIcon: const SizedBox(
-                        width: 26,
-                      ),
-                      border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(90),
-                        ),
-                        borderSide: BorderSide.none,
-                      ),
-                      hintStyle:
-                          ArchethicThemeStyles.textStyleSize12W100Primary,
-                      filled: true,
-                      hintText: localizations.searchField,
+                child: TextFormField(
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.zero,
+                    prefixIcon: Icon(
+                      Symbols.search,
+                      color: ArchethicTheme.text,
+                      size: 18,
+                      weight: IconSize.weightM,
+                      opticalSize: IconSize.opticalSizeM,
+                      grade: IconSize.gradeM,
                     ),
-                    style: ArchethicThemeStyles.textStyleSize12W100Primary,
-                    textAlign: TextAlign.left,
-                    autocorrect: false,
-                    cursorColor: ArchethicTheme.text,
-                    inputFormatters: <TextInputFormatter>[
-                      UpperCaseTextFormatter(),
-                    ],
-                    onChanged: (text) async {
-                      setState(
-                        () {
-                          searchCriteria = text;
-                        },
-                      );
-                    },
+                    suffixIcon: const SizedBox(
+                      width: 26,
+                    ),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(90),
+                      ),
+                      borderSide: BorderSide.none,
+                    ),
+                    hintStyle: ArchethicThemeStyles.textStyleSize12W100Primary,
+                    filled: true,
+                    hintText: localizations.searchField,
                   ),
+                  style: ArchethicThemeStyles.textStyleSize12W100Primary,
+                  textAlign: TextAlign.left,
+                  autocorrect: false,
+                  cursorColor: ArchethicTheme.text,
+                  inputFormatters: <TextInputFormatter>[
+                    UpperCaseTextFormatter(),
+                  ],
+                  onChanged: (text) async {
+                    setState(
+                      () {
+                        searchCriteria = text;
+                      },
+                    );
+                  },
                 ),
               ),
             ),
-            const TokenAddBtn(),
+            if (FeatureFlags.tokenFungibleCreationFeature) const TokenAddBtn(),
           ],
         ),
         if (tokens != null)
