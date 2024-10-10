a0a11c4728260989852ba57e94446fd7678dd744
redDwarf03 <sylvain@archethic.net>
2024-10-10 16:20:13 +0200

    chore: :lipstick: Add background in accounts list modal

--- lib/ui/views/accounts/layouts/account_list.dart
+++ lib/ui/views/accounts/layouts/account_list.dart
@@ -34,71 +34,49 @@ class AccountsListState extends ConsumerState<AccountsList>
         ref.watch(AccountProviders.sortedAccounts).valueOrNull ?? [];
 
     return Expanded(
-      child: Stack(
-        children: [
-          ScrollConfiguration(
-            behavior: ScrollConfiguration.of(context).copyWith(
-              dragDevices: {
-                PointerDeviceKind.touch,
-                PointerDeviceKind.mouse,
-                PointerDeviceKind.trackpad,
-              },
-            ),
-            child: ListView.builder(
-              physics: const AlwaysScrollableScrollPhysics(),
-              padding: EdgeInsets.only(
-                top: MediaQuery.of(context).padding.top + 20,
-                bottom: MediaQuery.of(context).padding.bottom + 70,
-              ),
-              itemCount: accountsList.length,
-              itemBuilder: (BuildContext context, int index) {
-                Widget item = AccountListItem(
-                  account: accountsList[index],
-                  selectedAccount: selectedAccount,
-                );
+      child: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(
+          dragDevices: {
+            PointerDeviceKind.touch,
+            PointerDeviceKind.mouse,
+            PointerDeviceKind.trackpad,
+          },
+        ),
+        child: ListView.builder(
+          physics: const AlwaysScrollableScrollPhysics(),
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).padding.top + 20,
+            bottom: MediaQuery.of(context).padding.bottom + 70,
+          ),
+          itemCount: accountsList.length,
+          itemBuilder: (BuildContext context, int index) {
+            Widget item = AccountListItem(
+              account: accountsList[index],
+              selectedAccount: selectedAccount,
+            );
 
-                if (!kIsWeb) {
-                  item = item
-                      .animate(delay: 100.ms)
-                      .fadeIn(duration: 300.ms, delay: 200.ms)
-                      .shimmer(
-                        blendMode: BlendMode.srcOver,
-                        color: Colors.white12,
-                      )
-                      .move(
-                        begin: const Offset(-16, 0),
-                        curve: Curves.easeOutQuad,
-                      );
-                }
-                return ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                    child: item,
-                  ),
-                );
-              },
-            ),
-          ),
-          Positioned(
-            top: -10,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: 30,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Colors.black,
-                    Colors.black.withOpacity(0),
-                  ],
-                ),
+            if (!kIsWeb) {
+              item = item
+                  .animate(delay: 100.ms)
+                  .fadeIn(duration: 300.ms, delay: 200.ms)
+                  .shimmer(
+                    blendMode: BlendMode.srcOver,
+                    color: Colors.white12,
+                  )
+                  .move(
+                    begin: const Offset(-16, 0),
+                    curve: Curves.easeOutQuad,
+                  );
+            }
+            return ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: item,
               ),
-            ),
-          ),
-        ],
+            );
+          },
+        ),
       ),
     );
   }
