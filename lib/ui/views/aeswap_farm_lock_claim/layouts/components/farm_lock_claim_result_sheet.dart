0189871e93cd0362760d284662c4b86d0c505142
redDwarf03 <sylvain@archethic.net>
2025-03-04 18:57:46 +0100

    chore: :lipstick: Claim UX/UI updates

--- lib/ui/views/aeswap_farm_lock_claim/layouts/components/farm_lock_claim_result_sheet.dart
+++ lib/ui/views/aeswap_farm_lock_claim/layouts/components/farm_lock_claim_result_sheet.dart
@@ -1,18 +1,13 @@
 import 'package:aewallet/application/account/accounts_notifier.dart';
-import 'package:aewallet/modules/aeswap/ui/views/util/app_styles.dart';
 import 'package:aewallet/modules/aeswap/ui/views/util/components/failure_message.dart';
 import 'package:aewallet/modules/aeswap/ui/views/util/components/format_address_link_copy_big_icon.dart';
-import 'package:aewallet/ui/util/dimens.dart';
+import 'package:aewallet/ui/figma_components/buttons/btn_footer_primary.dart';
+import 'package:aewallet/ui/figma_components/message_box/message_box.dart';
 import 'package:aewallet/ui/views/aeswap_farm_lock_claim/bloc/provider.dart';
 import 'package:aewallet/ui/views/aeswap_farm_lock_claim/layouts/components/farm_lock_claim_final_amount.dart';
 import 'package:aewallet/ui/views/main/components/sheet_appbar.dart';
-import 'package:aewallet/ui/widgets/components/app_button_tiny.dart';
-import 'package:aewallet/ui/widgets/components/sheet_detail_card.dart';
 import 'package:aewallet/ui/widgets/components/sheet_skeleton.dart';
 import 'package:aewallet/ui/widgets/components/sheet_skeleton_interface.dart';
-import 'package:archethic_dapp_framework_flutter/archethic_dapp_framework_flutter.dart'
-    as aedappfm;
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -52,20 +47,15 @@ class FarmLockClaimResultSheetState
 
   @override
   Widget getFloatingActionButton(BuildContext context, WidgetRef ref) {
-    return Row(
-      children: <Widget>[
-        AppButtonTinyConnectivity(
-          AppLocalizations.of(context)!.close,
-          Dimens.buttonBottomDimens,
-          key: const Key('close'),
-          onPressed: () async {
-            ref.invalidate(farmLockClaimFormNotifierProvider);
-            context
-              ..pop()
-              ..pop();
-          },
-        ),
-      ],
+    return BtnFooterPrimary(
+      buttonText: AppLocalizations.of(context)!.close,
+      key: const Key('close'),
+      onTap: () async {
+        ref.invalidate(farmLockClaimFormNotifierProvider);
+        context
+          ..pop()
+          ..pop();
+      },
     );
   }
 
@@ -96,45 +86,19 @@ class FarmLockClaimResultSheetState
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SheetDetailCard(
-              children: [
-                if (finalAmount == null)
-                  if (timeout == false)
-                    Row(
-                      children: [
-                        AutoSizeText(
-                          AppLocalizations.of(context)!.processingInProgress,
-                          style: AppTextStyles.bodyLarge(context),
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                          width: 10,
-                          child: CircularProgressIndicator(strokeWidth: 1),
-                        ),
-                      ],
-                    )
-                  else
-                    Text(
-                      FailureMessage(
-                        context: context,
-                        failure: farmLockClaim.failure,
-                      ).getMessage(),
-                      style: AppTextStyles.bodyLarge(context).copyWith(
-                        color: aedappfm.ArchethicThemeBase.systemDanger500,
-                      ),
-                    )
-                else
-                  Text(
-                    AppLocalizations.of(context)!.farmLockClaimSuccessInfo,
-                    style: AppTextStyles.bodyLarge(context).copyWith(
-                      color: aedappfm.ArchethicThemeBase.systemPositive600,
-                    ),
-                  ),
-              ],
-            ),
+            if (finalAmount == null)
+              MessageBox(
+                messageBoxType: MessageBoxType.warning,
+                text: FailureMessage(
+                  context: context,
+                  failure: farmLockClaim.failure,
+                ).getMessage(),
+              )
+            else
+              MessageBox(
+                messageBoxType: MessageBoxType.success,
+                text: AppLocalizations.of(context)!.liquidityAddSuccessInfo,
+              ),
             const SizedBox(
               height: 20,
             ),
@@ -162,11 +126,7 @@ class FarmLockClaimResultSheetState
                 ],
               ),
             if (finalAmount != null || timeout)
-              const SheetDetailCard(
-                children: [
-                  FarmLockClaimFinalAmount(),
-                ],
-              ),
+              const FarmLockClaimFinalAmount(),
           ],
         ),
       ),
