f1b354b867cb8fbd65a7732100a1bf4764c63abc
redDwarf03 <sylvain@archethic.net>
2025-03-05 09:01:18 +0100

    chore: :lipstick: Update Level up UX/UI

--- lib/ui/views/aeswap_farm_lock_level_up/layouts/components/farm_lock_level_up_result_sheet.dart
+++ lib/ui/views/aeswap_farm_lock_level_up/layouts/components/farm_lock_level_up_result_sheet.dart
@@ -1,18 +1,13 @@
 import 'package:aewallet/application/account/accounts_notifier.dart';
-import 'package:aewallet/modules/aeswap/ui/views/util/app_styles.dart';
 import 'package:aewallet/modules/aeswap/ui/views/util/components/failure_message.dart';
 import 'package:aewallet/modules/aeswap/ui/views/util/components/format_address_link_copy_big_icon.dart';
-import 'package:aewallet/ui/util/dimens.dart';
+import 'package:aewallet/ui/figma_components/buttons/btn_footer_primary.dart';
+import 'package:aewallet/ui/figma_components/message_box/message_box.dart';
 import 'package:aewallet/ui/views/aeswap_farm_lock_level_up/bloc/provider.dart';
 import 'package:aewallet/ui/views/aeswap_farm_lock_level_up/layouts/components/farm_lock_level_up_final_amount.dart';
 import 'package:aewallet/ui/views/main/components/sheet_appbar.dart';
-import 'package:aewallet/ui/widgets/components/app_button_tiny.dart';
-import 'package:aewallet/ui/widgets/components/sheet_detail_card.dart';
 import 'package:aewallet/ui/widgets/components/sheet_skeleton.dart';
 import 'package:aewallet/ui/widgets/components/sheet_skeleton_interface.dart';
-import 'package:archethic_dapp_framework_flutter/archethic_dapp_framework_flutter.dart'
-    as aedappfm;
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -52,20 +47,15 @@ class FarmLockLevelUpResultSheetState
 
   @override
   Widget getFloatingActionButton(BuildContext context, WidgetRef ref) {
-    return Row(
-      children: <Widget>[
-        AppButtonTinyConnectivity(
-          AppLocalizations.of(context)!.close,
-          Dimens.buttonBottomDimens,
-          key: const Key('close'),
-          onPressed: () async {
-            ref.invalidate(farmLockLevelUpFormNotifierProvider);
-            context
-              ..pop()
-              ..pop();
-          },
-        ),
-      ],
+    return BtnFooterPrimary(
+      buttonText: AppLocalizations.of(context)!.close,
+      key: const Key('close'),
+      onTap: () async {
+        ref.invalidate(farmLockLevelUpFormNotifierProvider);
+        context
+          ..pop()
+          ..pop();
+      },
     );
   }
 
@@ -96,45 +86,19 @@ class FarmLockLevelUpResultSheetState
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SheetDetailCard(
-              children: [
-                if (finalAmount == null)
-                  if (timeout == false)
-                    Row(
-                      children: [
-                        AutoSizeText(
-                          AppLocalizations.of(context)!.processingInProgress,
-                          style: AppTextStyles.bodyLarge(context),
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        const SizedBox(
-                          height: 10,
-                          width: 10,
-                          child: CircularProgressIndicator(strokeWidth: 1),
-                        ),
-                      ],
-                    )
-                  else
-                    Text(
-                      FailureMessage(
-                        context: context,
-                        failure: farmLockLevelUp.failure,
-                      ).getMessage(),
-                      style: AppTextStyles.bodyLarge(context).copyWith(
-                        color: aedappfm.ArchethicThemeBase.systemDanger500,
-                      ),
-                    )
-                else
-                  AutoSizeText(
-                    AppLocalizations.of(context)!.levelUpFarmLockDone,
-                    style: AppTextStyles.bodyLarge(context).copyWith(
-                      color: aedappfm.ArchethicThemeBase.systemPositive600,
-                    ),
-                  ),
-              ],
-            ),
+            if (finalAmount == null)
+              MessageBox(
+                messageBoxType: MessageBoxType.warning,
+                text: FailureMessage(
+                  context: context,
+                  failure: farmLockLevelUp.failure,
+                ).getMessage(),
+              )
+            else
+              MessageBox(
+                messageBoxType: MessageBoxType.success,
+                text: AppLocalizations.of(context)!.levelUpFarmLockDone,
+              ),
             const SizedBox(
               height: 20,
             ),
@@ -162,11 +126,7 @@ class FarmLockLevelUpResultSheetState
                 ],
               ),
             if (finalAmount != null || timeout)
-              const SheetDetailCard(
-                children: [
-                  FarmLockLevelUpFinalAmount(),
-                ],
-              ),
+              const FarmLockLevelUpFinalAmount(),
           ],
         ),
       ),
