c6b1ddca203f496b9b54f0a321d8deaefaa46775
redDwarf03 <sylvain@archethic.net>
2025-03-24 22:09:01 +0100

    chore: Move intercom link to bottom page

--- lib/application/intercom.dart
+++ lib/application/intercom.dart
@@ -7,16 +7,12 @@ part 'intercom.g.dart';
 
 @riverpod
 bool isIntercomEnabled(Ref ref) {
-  return UniversalPlatform.isAndroid ||
-      UniversalPlatform.isIOS ||
-      UniversalPlatform.isWeb;
+  return UniversalPlatform.isAndroid || UniversalPlatform.isIOS;
 }
 
 @riverpod
 Future<bool> isIntercomConnected(Ref ref) async {
-  if (UniversalPlatform.isAndroid ||
-      UniversalPlatform.isIOS ||
-      UniversalPlatform.isWeb) {
+  if (UniversalPlatform.isAndroid || UniversalPlatform.isIOS) {
     return Intercom.instance.isUserLoggedIn();
   }
   return false;
@@ -28,9 +24,7 @@ Future<void> connectIntercom(Ref ref) async {
       await ref.watch(isIntercomConnectedProvider.future);
   if (isIntercomConnected) return;
 
-  if (UniversalPlatform.isAndroid ||
-      UniversalPlatform.isIOS ||
-      UniversalPlatform.isWeb) {
+  if (UniversalPlatform.isAndroid || UniversalPlatform.isIOS) {
     const appId = String.fromEnvironment('INTERCOM_APP_ID');
     const androidKey = String.fromEnvironment('INTERCOM_ANDROID_KEY');
     const iOSKey = String.fromEnvironment('INTERCOM_IOS_KEY');
@@ -44,9 +38,7 @@ Future<void> connectIntercom(Ref ref) async {
 
 @riverpod
 Future<void> disconnectIntercom(Ref ref) async {
-  if (UniversalPlatform.isAndroid ||
-      UniversalPlatform.isIOS ||
-      UniversalPlatform.isWeb) {
+  if (UniversalPlatform.isAndroid || UniversalPlatform.isIOS) {
     await Intercom.instance.logout();
   }
 }
